60a3126ab2980b87f42cce867b18ab7e14448bcb
Austin Walters <lettergramm@gmail.com>
2021-02-23 11:19:55 -0600

    updated versioning mechanism

---
+++ _version.py
@@ -0,0 +1,12 @@
+"""
+File containers the version number for the package
+"""
+
+MAJOR               = 0
+MINOR               = 3
+MICRO               = 1
+ISRELEASED          = True
+
+VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+
+__version__ = VERSION

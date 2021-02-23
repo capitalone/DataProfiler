7f95a9f3fb752574ae68d29bb10e168c6c691e71
Austin Walters <lettergramm@gmail.com>
2021-02-23 12:08:40 -0600

    Add version.py to enable version number from package  (#27)

---
+++ dataprofiler/version.py
@@ -0,0 +1,11 @@
+"""
+File containers the version number for the package
+"""
+
+MAJOR               = 0
+MINOR               = 3
+MICRO               = 1
+
+VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+
+__version__ = VERSION

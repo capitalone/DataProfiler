6066bf6e070fd36971667b0cd7f048021f61922d
Sagar Saxena <sagardsaxena@gmail.com>
2021-02-26 16:12:17 -0500

    Add Base Column Options Tests

---
+++ dataprofiler/tests/profilers/test_base_column_options.py
@@ -0,0 +1,47 @@
+import os
+import unittest
+from unittest import mock
+
+import pandas as pd
+
+from dataprofiler import Data, ProfilerOptions, Profiler
+from dataprofiler.profilers.profiler_options import BaseColumnOptions
+from dataprofiler.tests.profilers.test_boolean_option import TestBooleanOption
+
+
+@mock.patch('dataprofiler.profilers.data_labeler_column_profile.'
+            'DataLabelerColumn.update', return_value=None)
+@mock.patch('dataprofiler.profilers.data_labeler_column_profile.DataLabeler')
+class TestBaseColumnOptions(TestBooleanOption):
+	
+	@classmethod
+	def setUpClass(cls):
+		cls.data = Data(data=pd.DataFrame([1, 2]), data_type='csv')
+	
+	def sanity(self, *mocks):
+		option = BaseColumnOption()
+		self.assertTrue("is_enabled" in option.properties)
+	
+	def test_set_helper(self, *mocks):
+		super().test_set_helper(*mocks)
+
+	def test_set(self, *mocks):
+		super().test_set(*mocks)
+	
+	def test_validate_helper(self, *mocks):
+		super().test_validate_helper(*mocks)
+	
+	def test_validate(self, *mocks):
+		super().test_validate(*mocks)
+
+	def test_is_prop_enabled(self, *mocks):
+		options = BaseColumnOptions()
+
+		self.assertTrue(options.is_prop_enabled("is_enabled"))
+		options.set({"is_enabled": False})
+		self.assertFalse(options.is_prop_enabled("is_enabled"))
+		
+		expected_error = 'Property "Hello World" does not exist in BaseColumnOptions.'
+		with self.assertRaisesRegex(AttributeError, expected_error):
+			options.is_prop_enabled("Hello World")
+

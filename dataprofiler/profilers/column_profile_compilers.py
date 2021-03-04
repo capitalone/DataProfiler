068933c1c97d3a1a6d8b3d0f4d8fed2864581dcf
lettergram <lettergramm@gmail.com>
2021-03-04 10:45:02 -0600

    updated error messages

--- dataprofiler/profilers/column_profile_compilers.py
+++ dataprofiler/profilers/column_profile_compilers.py
@@ -71,9 +71,8 @@ class BaseColumnProfileCompiler(with_metaclass(abc.ABCMeta, object)):
                         raise ValueError(e)
                     
                     warning_msg += "\n\nFor labeler errors, try installing "
-                    warning_msg += "the ml extra; command:\n\n"
-                    warning_msg += "  $ pip install dataprofiler[ml]"
-                    warning_msg += " --user\n\n"
+                    warning_msg += "the extra ml requirements via:\n\n"
+                    warning_msg += "$ pip install dataprofiler[ml] --user\n\n"
 
                     warnings.warn(warning_msg, RuntimeWarning, stacklevel=2)
             

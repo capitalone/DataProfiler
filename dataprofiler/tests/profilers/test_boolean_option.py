55f1e345e319f7405fe6572ec4dd76a26c51eee6
Sagar Saxena <sagardsaxena@gmail.com>
2021-03-12 15:33:48 -0500

    Update test_init and test_set_helper

--- dataprofiler/tests/profilers/test_boolean_option.py
+++ dataprofiler/tests/profilers/test_boolean_option.py
@@ -24,10 +24,10 @@ class TestBooleanOption(TestBaseOption):
         super().test_set_helper(*mocks)
         
         # Enable and Disable Option
-        option = self.get_options(is_enabled=False)
-        self.assertFalse(option.properties['is_enabled'])        
-        option._set_helper({'is_enabled':True}, '') 
-        self.assertTrue(option.properties['is_enabled'])        
+        for is_enabled in [True, False]:
+            option = self.get_options(is_enabled=not is_enabled)
+            option.set({'is_enabled':is_enabled}) 
+            self.assertEqual(is_enabled, option.properties['is_enabled'])        
 
         # Treat is_enabled as a BooleanOption
         expected_error = "type object 'is_enabled' has no attribute 'is_enabled'"

f63cad6861fdc0bab8c675915c81118163bb3159
Austin Walters <lettergramm@gmail.com>
2021-03-16 16:05:45 -0500

    Update version (#72)
    * improved hashing functionality
    
    * impoved unique detection
    
    * updated to use range generator
    
    * much faster
    
    * remove duplicates
    
    * Update version.py

--- dataprofiler/version.py
+++ dataprofiler/version.py
@@ -4,7 +4,7 @@ File containers the version number for the package
 
 MAJOR               = 0
 MINOR               = 3
-MICRO               = 4
+MICRO               = 5
 
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 

f3b1c43a1224951251162bf05a2cc9583ef0047f
Sagar Saxena <sagardsaxena@gmail.com>
2021-03-22 18:29:55 -0400

    Add tests for Numerical, Int, Float, and Text Options (#79)
    * Add Initial Base Options Test
    
    * Rename From Base Options to Base Option
    
    * Add Tests For Boolean Option
    
    * Add Base Column Options Tests
    
    * Modify Sanity Test
    
    * Type Check Variable Path in BaseOption Class
    
    * Add Initial Tests For Numerical Options
    
    * Set With Invalid Option Name
    
    * Add Numeric Stats Enabled Tests
    
    * Test Boolean Option Variable Path
    
    * Test Boolean Option Variable Path
    
    * Add getOptions and getOptionsPath methods
    
    * Add validate, validate helper, and init tests
    
    * Add class methods and test_init
    
    * Fix Init Test
    
    * Fix Spacing
    
    * Fix formatting issues
    
    * Add initial params for super classes
    
    * Use correct default variable path
    
    * Add comments
    
    * Replace assertEqual with assertTrue/False
    
    * Fix formatting and add class methods
    
    * Enable before checking is enabled
    
    * Fix validate tests for multiple is_enabled
    
    * Add initial tests for IntOptions
    
    * Fix class name
    
    * Add initial tests for FloatOptions
    
    * Fix variable path for FloatOptions precision
    
    * Only use numeric keys for is_numeric_stats_enabled test
    
    * Fix Super Class
    
    * Add initial tests for TextOptions
    
    * Use generic constructor
    
    * Remove redundant type check
    
    * Add AbstractTestOptions class and infer variable path
    
    * Replace tabs with spaces
    
    * Add default and type check for options_class
    
    * Add required methods for tests
    
    * Remove DataLabeler mocks
    
    * Type check options_class before creating object
    
    * Change assertEqual to assertTrue/False
    
    * Update test_init and test_set_helper
    
    * Update test_init and test_set_helper
    
    * Use _set_helper instead of set in test_set_helper
    
    * Fix validate_options_class call
    
    * Return None on validate w/ no errors
    
    * Return None on validate w/ no errors
    
    * Revert incorrect merge resolution
    
    * Revert incorrect merge resolution
    
    * Fix bugs and formatting
    
    * Fix formatting and remove redundant code
    
    * Fix formatting and remove redundant code
    
    * Remove unused imports
    
    * Move option_class outside of setUpClass
    
    * Move keys outside of setUpClass
    
    * Fix Spacing
    
    * Use TestNumericalOptions.keys
    
    * Remove comments
    
    Co-authored-by: Austin Walters <lettergramm@gmail.com>

---
+++ dataprofiler/tests/profilers/test_numerical_options.py
@@ -0,0 +1,103 @@
+from dataprofiler.profilers.profiler_options import BooleanOption, NumericalOptions
+from dataprofiler.tests.profilers.test_base_column_options import TestBaseColumnOptions
+
+
+class TestNumericalOptions(TestBaseColumnOptions):
+    
+    option_class = NumericalOptions
+    keys = ["min", "max", "sum", "variance", "histogram_and_quantiles"]
+
+    def test_init(self, *mocks):
+        options = self.get_options()
+        for key in self.keys + ['is_numeric_stats_enabled', 'is_enabled']:
+            self.assertTrue(key in options.properties)
+    
+    def test_set_helper(self, *mocks):
+        super().test_set_helper(*mocks)
+        options = self.get_options()
+
+        # Enable and Disable Options
+        for key in self.keys:
+            skey = '{}.is_enabled'.format(key)
+            for enabled in [True, False]:
+                options._set_helper({skey:enabled}, '') 
+                self.assertEqual(enabled, options.properties[key].is_enabled)        
+
+    def test_set(self, *mocks):
+        super().test_set(*mocks)
+        options = self.get_options()
+        
+        # Enable and Disable Options
+        for key in self.keys:
+            skey = '{}.is_enabled'.format(key)
+            for enabled in [True, False]:
+                options.set({skey:enabled}) 
+                self.assertEqual(enabled, options.properties[key].is_enabled)        
+    
+    def test_validate_helper(self, *mocks):
+        super().test_validate_helper(*mocks)
+        options = self.get_options()
+        optpth = self.get_options_path()
+    
+        # Set BooleanOptions' is_enabled to a non-boolean value
+        for key in self.keys:
+            skey = '{}.is_enabled'.format(key)
+            expected_error = "{}.{}.is_enabled must be a Boolean.".format(optpth, key)
+            
+            options.set({skey: "Hello World"})
+            self.assertEqual([expected_error], options._validate_helper())
+            options.set({skey: True})
+
+        # Disable Sum and Enable Variance
+        options.set({"sum.is_enabled": False, "variance.is_enabled": True})
+        expected_error = "{}: The numeric stats must toggle on the sum if the variance is toggled on.".format(optpth)
+        self.assertEqual([expected_error], options._validate_helper()) 
+    
+    def test_validate(self, *mocks):
+        super().test_validate(*mocks)
+        options = self.get_options()
+        optpth = self.get_options_path()
+        
+        # Set BooleanOptions' is_enabled to a non-boolean value
+        for key in self.keys:
+            skey = '{}.is_enabled'.format(key)
+            expected_error = "{}.{}.is_enabled must be a Boolean.".format(optpth, key)
+            
+            options.set({skey: "Hello World"})
+            with self.assertRaisesRegex(ValueError, expected_error):
+                options.validate(raise_error=True)    
+            self.assertEqual([expected_error], options.validate(raise_error=False))
+            options.set({skey: True})
+
+        # Disable Sum and Enable Variance
+        options.set({"sum.is_enabled": False, "variance.is_enabled": True})
+        expected_error = "{}: The numeric stats must toggle on the sum if the variance is toggled on.".format(optpth)
+        with self.assertRaisesRegex(ValueError, expected_error):
+            options.validate(raise_error=True)    
+        self.assertEqual([expected_error], options.validate(raise_error=False)) 
+    
+    def test_is_numeric_stats_enabled(self, *mocks):
+        options = self.get_options()
+        numeric_keys = ["min", "max", "sum", "variance", "histogram_and_quantiles"]    
+
+        # Disable All Numeric Stats
+        options.set({'{}.is_enabled'.format(key):False for key in numeric_keys})
+        self.assertFalse(options.is_numeric_stats_enabled)
+        
+        # Enable Only One Numeric Stat
+        for key in numeric_keys:
+            skey = '{}.is_enabled'.format(key)
+            options.set({skey: True})
+            self.assertTrue(options.is_numeric_stats_enabled)
+            options.set({skey: False})
+
+        # Enable All Numeric Stats
+        options.is_numeric_stats_enabled = True
+        for key in numeric_keys:            
+            self.assertTrue(options.is_numeric_stats_enabled)
+
+        # Disable All Numeric Stats
+        options.is_numeric_stats_enabled = False
+        for key in numeric_keys:            
+            self.assertFalse(options.is_numeric_stats_enabled)
+

3632ddb0582f89641add98e3c810cd14161d60e3
JGSweets <JGSweets@users.noreply.github.com>
2021-03-23 12:14:42 -0500

    Update precision calculation to address scientific notation, ints, and spaces (#81)
    * fix: precision calculation to include values before decimal
    
    * fix: series names
    
    * fix: add precision tests
    
    * fix: precision function description
    
    * fix: requested move from pandas empty

--- dataprofiler/tests/profilers/test_float_column_profile.py
+++ dataprofiler/tests/profilers/test_float_column_profile.py
@@ -53,11 +53,11 @@ class TestFloatColumn(unittest.TestCase):
         df_1 = pd.Series([0.4, 0.3, 0.1, 0.1, 0.1]).apply(str)
         df_2 = pd.Series([0.11, 0.11, 0.12, 2.11]).apply(str)
         df_3 = pd.Series([4.114, 3.161, 2.512, 2.131]).apply(str)
-        df_mix = pd.Series([4.1, 3., 2.52, 2.13143]).apply(str)
+        df_mix = pd.Series([4.1, '3.', 2.52, 2.13143]).apply(str)
 
         float_profiler = FloatColumn("Name")
         float_profiler.update(df_3)
-        self.assertEqual(3, float_profiler.precision)
+        self.assertEqual(4, float_profiler.precision)
 
         float_profiler.update(df_2)
         self.assertEqual(2, float_profiler.precision)
@@ -69,6 +69,57 @@ class TestFloatColumn(unittest.TestCase):
         float_profiler.update(df_mix)
         self.assertEqual(1, float_profiler.precision)
 
+        # edge cases #
+        # integer with 0s on right and left side
+        df_ints = pd.Series(['0013245678', '123456700', '0012345600'])
+        float_profiler = FloatColumn("Name")
+        float_profiler.update(df_ints)
+        self.assertEqual(6, float_profiler.precision)
+
+        # scientific
+        df_scientific = pd.Series(['1.23e-3', '2.2344', '1.244e4'])
+        float_profiler = FloatColumn("Name")
+        float_profiler.update(df_scientific)
+        self.assertEqual(3, float_profiler.precision)
+
+        # plus
+        df_plus = pd.Series(['+1.3e-3', '+2.244', '+1.3324e4'])
+        float_profiler = FloatColumn("Name")
+        float_profiler.update(df_plus)
+        self.assertEqual(2, float_profiler.precision)
+
+        # minus
+        df_minus = pd.Series(['-1.3234e-3', '-0.244', '-1.3324e4'])
+        float_profiler = FloatColumn("Name")
+        float_profiler.update(df_minus)
+        self.assertEqual(3, float_profiler.precision)
+
+        # spaces around values
+        df_spaces = pd.Series(['  -1.3234e-3  ', '  -0.244  '])
+        float_profiler = FloatColumn("Name")
+        float_profiler.update(df_spaces)
+        self.assertEqual(3, float_profiler.precision)
+
+        # check to make sure all formats of precision are correctly predicted
+        samples = [
+            # value, expected precision
+            ['10.01', 4],
+            ['.01', 1],
+            ['0.01', 1],
+            ['-0.01', 1],
+            ['+0.01', 1],
+            [' +0.013', 2],
+            ['  -1.3234e-3  ', 5],
+            ['  0012345600  ', 6],
+            ['  0012345600.  ', 8],
+            ['  -0012345600.  ', 8],
+        ]
+        for sample in samples:
+            df_series = pd.Series([sample[0]])
+            expected_precision = sample[1]
+            precision = FloatColumn._get_float_precision(df_series)
+            self.assertEqual(expected_precision, precision,
+                             msg='Errored for: {}'.format(sample[0]))
 
     def test_profiled_min(self):
         # test with multiple values

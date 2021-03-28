09b0b7b7ee6b530e31a59e6e3c70714a2cb0428c
lettergram <austin@agw.io>
2021-03-28 15:11:01 -0500

    updated _prepare_report

--- dataprofiler/profilers/helpers/report_helpers.py
+++ dataprofiler/profilers/helpers/report_helpers.py
@@ -41,7 +41,7 @@ def flat_dict(od, separator='_', key=''):
            } if isinstance(od, dict) else {key:od}
 
 
-def _prepare_report(report, output_format=None):
+def _prepare_report(report, output_format=None, omit_keys=[]):
     """
     Prepares report dictionary for users upon request
 
@@ -49,39 +49,52 @@ def _prepare_report(report, output_format=None):
     :type report: dict()
     :param output_format: designation for how to format the returned report
     :type output_format: dict()
+    :param omit_keys: Keys to omit from the output report
+    :type omit_keys: list(str)
     :return report: handle to the updated report
     :type report: dict()
     """
+    
+    format_options = ['pretty', 'serializable', 'flat']
     if output_format:
         output_format = output_format.lower()
-    if not output_format or output_format not in ['pretty', 'serializable', 'flat']:
+    if not output_format or output_format not in format_options:
         return report
-    report = report.copy()
+    
+    fmt_report = report.copy()
     max_str_len = 50
     max_array_len = 5
-    for key in report:
-        if isinstance(report[key], dict):
-            report[key] = _prepare_report(report[key], output_format=output_format)
-        elif isinstance(report[key], list) or isinstance(report[key], np.ndarray):
+    for key in fmt_report:
+        value = fmt_report[key]
+        if isinstance(value, dict):
+            fmt_report[key] = _prepare_report(
+                fmt_report[key], output_format=output_format)
+        elif isinstance(value, list) or isinstance(value, np.ndarray):
             if output_format == "pretty":
-                if isinstance(report[key], list):
-                    report[key] = np.array(report[key])
-                str_value = np.array2string(report[key], separator=', ')
-                if len(str_value) > max_str_len and len(report[key]) > max_array_len:
+                if isinstance(value, list):
+                    fmt_report[key] = np.array(value)
+                str_value = np.array2string(value, separator=', ')
+                if len(str_value) > max_str_len and len(value) > max_array_len:
                     ind = 1
                     str_value = ''
                     while len(str_value) <= max_str_len:
                         str_value = \
-                            np.array2string(report[key][:ind], separator=', ')[:-1] + \
+                            np.array2string(value[:ind], separator=', ')[:-1] + \
                             ', ... , ' + \
                             np.array2string(
-                                report[key][-ind:], separator=', ')[1:]
+                                value[-ind:], separator=', ')[1:]
                         ind += 1
-                report[key] = str_value
-            elif output_format == "serializable" and isinstance(report[key], np.ndarray):
-                report[key] = report[key].tolist()
-        elif output_format == "pretty" and isinstance(report[key], float):
-            report[key] = round(report[key], 4)
+                fmt_report[key] = str_value
+            elif output_format == "serializable" and isinstance(value, np.ndarray):
+                fmt_report[key] = value.tolist()
+        elif output_format == "pretty" and isinstance(value, float):
+            fmt_report[key] = round(fmt_report[key], 4)
+
+        # Remove any keys omitted
+        if key in omit_keys:
+            fmt_report.pop(key, None)
+            
     if output_format == 'flat':
-        report = flat_dict(report)
-    return report
+        fmt_report = flat_dict(fmt_report)
+        
+    return fmt_report

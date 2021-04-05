85ebc9ef5a41c1174fd868914f2f674203734d70
lettergram <lettergramm@gmail.com>
2021-04-05 10:27:34 -0500

    \s

--- dataprofiler/profilers/float_column_profile.py
+++ dataprofiler/profilers/float_column_profile.py
@@ -109,9 +109,9 @@ class FloatColumn(NumericStatsMixin, BaseColumnPrimitiveTypeProfiler):
         if not len(df_series_clean):
             return 0
 
-        # Lead zeros: ^[+-.0 \t\n]+ End zeros: \.?0+([ \n\t]|$)
+        # Lead zeros: ^[+-.0\s\t\n]+ End zeros: \.?0+([ \n\t]|$)
         # Scientific Notation: (?<=[e])(.*) Any non-digits: \D
-        r = re.compile('^[+-.0 \t\n]+|\.?0+([ \n\t]|$)|(?<=[e])(.*)|\D')
+        r = re.compile('^[+-.0\s\t\n]+|\.?0+([ \n\t]|$)|(?<=[e])(.*)|\D')
         
         return df_series_clean.replace(to_replace=r, value='').map(len).min()
         

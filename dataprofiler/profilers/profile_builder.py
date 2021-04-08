3fd62e5af7e60442e7e2648af00746457f470879
JGSweets <JGSweets@users.noreply.github.com>
2021-04-08 10:07:37 -0500

    Continued fixes for option passing on merge of profiles (#142)
    * fix: don't create profilers on merge, bc going to be overwriting them
    
    * fix: pass options on merge of StructuredOptions

--- dataprofiler/profilers/profile_builder.py
+++ dataprofiler/profilers/profile_builder.py
@@ -28,6 +28,7 @@ from .helpers.report_helpers import calculate_quantiles, _prepare_report
 from .profiler_options import ProfilerOptions, StructuredOptions, \
     DataLabelerOptions
 
+
 class StructuredDataProfile(object):
 
     def __init__(self, df_series, sample_size=None, min_sample_size=5000,
@@ -124,7 +125,9 @@ class StructuredDataProfile(object):
             min_sample_size=max(self._min_sample_size, other._min_sample_size),
             sampling_ratio=max(self._sampling_ratio, other._sampling_ratio),
             min_true_samples=max(self._min_true_samples,
-                                 other._min_true_samples))
+                                 other._min_true_samples),
+            options=self.options,
+        )
 
         merged_profile.name = self.name
         merged_profile._update_base_stats(

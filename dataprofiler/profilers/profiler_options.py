b05253d99c390ba6cf45dab15b8d133cb2028662
lettergram <lettergramm@gmail.com>
2021-04-14 01:01:48 -0500

    removed error catching

--- dataprofiler/profilers/profiler_options.py
+++ dataprofiler/profilers/profiler_options.py
@@ -424,16 +424,16 @@ class FloatOptions(NumericalOptions):
                           .format(variable_path))        
         errors += self.precision._validate_helper(
             variable_path + '.precision')
-
-        """
+        
+        
         if self.precision_sample_ratio is not None \
            and isinstance(self.precision_sample_ratio, float) \
            and self.precision_sample_ratio < 0 and self.precision_sample_ratio > 1.0:
             errors.append("{}.precision_sample_ratio must be a float between 0 and 1."
                           .format(variable_path))
-        errors += self.precision._validate_helper(
-            variable_path + '.precision_sample_ratio')
-        """
+        #errors += self.precision._validate_helper(
+        # variable_path + '.precision_sample_ratio')
+        
         return errors
 
 

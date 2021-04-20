76922c63fac647e04865eaf0287eca8e757a556e
ChrisWallace2020 <chriswallace585@gmail.com>
2021-04-20 10:42:56 -0400

    Implimented null row index trimming by min sampled id, no test written yet

--- dataprofiler/profilers/profile_builder.py
+++ dataprofiler/profilers/profile_builder.py
@@ -544,8 +544,8 @@ class Profiler(object):
             pd.util.hash_pandas_object(data, index=False), True
         )
 
-        min_sample_id = min([self._profile[col].sample_ids[-1]
-                             for col in self._profile], default=None)
+        min_id = min([self._profile[col].sample_ids[-1]
+                      for col in self._profile])
 
         # Calculate Null Column Count
         null_rows = set()
@@ -556,6 +556,10 @@ class Profiler(object):
             null_row_indices = set()
             if null_type_dict:
                 null_row_indices = set.union(*null_type_dict.values())
+
+            # Take null indices up to minimum samples taken across all columns
+            trimmed_sample_ids = self._profile[column].sample_ids[:min_id + 1]
+            null_row_indices = null_row_indices.intersection(trimmed_sample_ids)
 
             # Find the common null indices between the columns
             if first_col_flag:

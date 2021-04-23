9d767a312d28ba2ff8585cc8af080cbda8d22272
JGSweets <JGSweets@users.noreply.github.com>
2021-04-23 11:58:25 -0500

    fixes the len data error if data sent in is  (#184)
    * fix: len data issue
    
    * fix: add none check in profiler init

--- dataprofiler/profilers/profile_builder.py
+++ dataprofiler/profilers/profile_builder.py
@@ -445,7 +445,7 @@ class Profiler(object):
                 utils.warn_on_profile('data_labeler', e)
                 self.options.set({'data_labeler.is_enabled': False})
 
-        if len(data):
+        if data is not None:
             self.update_profile(data)
 
     def __add__(self, other):
@@ -658,6 +658,8 @@ class Profiler(object):
                 "pd.DataFrame."
             )
 
+        if not len(data):
+            return
         if not min_true_samples:
             min_true_samples = self._min_true_samples
         if not sample_size:

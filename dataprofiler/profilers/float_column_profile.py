Fix edge case errors in precision

dataprofiler/profilers/float_column_profile.py
@@ -38,14 +38,16 @@ class FloatColumn(NumericStatsMixin, BaseColumnPrimitiveTypeProfiler):
             'mean': None,
             'var': None,
             'std': None,
-            'biased_skewness': None,
-            'biased_kurtosis': None,
+            'skewness': None,
+            'kurtosis': None,
             'sum': None,
             'sample_size': None,
             'margin_of_error': None,
             'confidence_level': 0.999
         }
-        
+        self._precision_biased_skewness = np.nan
+        self._precision_biased_kurtosis = np.nan
+
         # https://www.calculator.net/confidence-interval-calculator.html
         self.__z_value_precision = 3.291
 
@@ -108,22 +110,30 @@ class FloatColumn(NumericStatsMixin, BaseColumnPrimitiveTypeProfiler):
                 merged_profile.precision['std'] = math.sqrt(
                     merged_profile.precision['var'])
 
-                merged_profile.precision['biased_skewness'] = self._merge_biased_skewness(
-                    self.precision['sample_size'], self.precision['biased_skewness'],
+                merged_profile._precision_biased_skewness = self._merge_biased_skewness(
+                    self.precision['sample_size'], self._precision_biased_skewness,
                     self.precision['var'], self.precision['mean'],
                     other.precision['sample_size'],
-                    self.precision['biased_skewness'], self.precision['var'],
-                    self.precision['mean'])
+                    other._precision_biased_skewness, other.precision['var'],
+                    other.precision['mean'])
 
-                merged_profile.precision['biased_kurtosis'] = self._merge_biased_kurtosis(
+                merged_profile.precision['skewness'] = self._correct_bias_skewness(
+                    merged_profile.precision['sample_size'],
+                    merged_profile._precision_biased_skewness)
+
+                merged_profile._precision_biased_kurtosis = self._merge_biased_kurtosis(
                     self.precision['sample_size'],
-                    self.precision['biased_kurtosis'],
-                    self.precision['biased_skewness'], self.precision['var'],
+                    self._precision_biased_kurtosis,
+                    self._precision_biased_skewness, self.precision['var'],
                     self.precision['mean'],
                     other.precision['sample_size'],
-                    other.precision['biased_kurtosis'],
-                    other.precision['biased_skewness'], other.precision['var'],
+                    other._precision_biased_kurtosis,
+                    other._precision_biased_skewness, other.precision['var'],
                     other.precision['mean'])
+
+                merged_profile.precision['kurtosis'] = self._correct_bias_kurtosis(
+                    merged_profile.precision['sample_size'],
+                    merged_profile._precision_biased_kurtosis)
 
                 # Margin of error, 99.9% confidence level
                 merged_profile.precision['margin_of_error'] = \
@@ -131,7 +141,8 @@ class FloatColumn(NumericStatsMixin, BaseColumnPrimitiveTypeProfiler):
                         merged_profile.precision['std']
                         / math.sqrt(merged_profile.precision['sample_size'])
                 )
-            
+
+
         return merged_profile
 
     @property
@@ -147,8 +158,8 @@ class FloatColumn(NumericStatsMixin, BaseColumnPrimitiveTypeProfiler):
             mean=self.np_type_to_type(self.mean),
             variance=self.np_type_to_type(self.variance),
             stddev=self.np_type_to_type(self.stddev),
-            skewness=self.skewness,
-            kurtosis=self.kurtosis,
+            skewness=self.np_type_to_type(self.skewness),
+            kurtosis=self.np_type_to_type(self.kurtosis),
             histogram=self._get_best_histogram_for_profile(),
             quantiles=self.quantiles,
             times=self.times,
@@ -158,12 +169,8 @@ class FloatColumn(NumericStatsMixin, BaseColumnPrimitiveTypeProfiler):
                 mean=self.np_type_to_type(self.precision['mean']),
                 var=self.np_type_to_type(self.precision['var']),
                 std=self.np_type_to_type(self.precision['std']),
-                skewness=self.np_type_to_type(self._correct_bias_skewness(
-                                              self.precision['sample_size'],
-                                              self.precision['biased_skewness'])),
-                kurtosis=self.np_type_to_type(self._correct_bias_kurtosis(
-                                              self.precision['sample_size'],
-                                              self.precision['biased_kurtosis'])),
+                skewness=self.np_type_to_type(self.precision['skewness']),
+                kurtosis=self.np_type_to_type(self.precision['kurtosis']),
                 sample_size=self.np_type_to_type(self.precision['sample_size']),
                 margin_of_error=self.np_type_to_type(self.precision['margin_of_error']),
                 confidence_level=self.np_type_to_type(self.precision['confidence_level'])
@@ -262,7 +269,6 @@ class FloatColumn(NumericStatsMixin, BaseColumnPrimitiveTypeProfiler):
         :type df_series: pandas.DataFrame
         :return: None
         """
-
         sample_ratio = None
         if self.__precision_sample_ratio is not None:
             sample_ratio = self.__precision_sample_ratio
@@ -272,8 +278,10 @@ class FloatColumn(NumericStatsMixin, BaseColumnPrimitiveTypeProfiler):
         if subset_precision is None:
             return
         elif self.precision['min'] is None:
+            self._precision_biased_kurtosis = subset_precision.pop('biased_kurtosis')
+            self._precision_biased_skewness = subset_precision.pop('biased_skewness')
             self.precision.update(subset_precision)
-        else:        
+        else:
             # Update the calculations as data is valid
             self.precision['min'] = min(
                 self.precision['min'], subset_precision['min'])
@@ -281,10 +289,10 @@ class FloatColumn(NumericStatsMixin, BaseColumnPrimitiveTypeProfiler):
                 self.precision['max'], subset_precision['max'])            
             self.precision['sum'] += subset_precision['sum']
 
-            self.precision['biased_kurtosis'] = self._merge_biased_kurtosis(
+            self._precision_biased_kurtosis = self._merge_biased_kurtosis(
                 self.precision['sample_size'],
-                self.precision['biased_kurtosis'],
-                self.precision['biased_skewness'], self.precision['var'],
+                self._precision_biased_kurtosis,
+                self._precision_biased_skewness, self.precision['var'],
                 self.precision['mean'],
                 subset_precision['sample_size'],
                 subset_precision['biased_kurtosis'],
@@ -292,9 +300,9 @@ class FloatColumn(NumericStatsMixin, BaseColumnPrimitiveTypeProfiler):
                 subset_precision['var'],
                 subset_precision['mean'])
 
-            self.precision['biased_skewness'] = self._merge_biased_skewness(
+            self._precision_biased_skewness = self._merge_biased_skewness(
                 self.precision['sample_size'],
-                self.precision['biased_skewness'], self.precision['var'],
+                self._precision_biased_skewness, self.precision['var'],
                 self.precision['mean'],
                 subset_precision['sample_size'],
                 subset_precision['biased_skewness'],
@@ -323,7 +331,14 @@ class FloatColumn(NumericStatsMixin, BaseColumnPrimitiveTypeProfiler):
         for key in ['mean', 'var', 'std', 'margin_of_error']:
             self.precision[key] = \
                 float('{:.{p}g}'.format(self.precision[key], p=sigfigs))
-                        
+
+        self.precision['skewness'] = self._correct_bias_skewness(
+            self.precision['sample_size'],
+            self._precision_biased_skewness)
+        self.precision['kurtosis'] = self._correct_bias_kurtosis(
+            self.precision['sample_size'],
+            self._precision_biased_kurtosis)
+
     def _update_helper(self, df_series_clean, profile):
         """
         Method for updating the column profile properties with a cleaned

dataprofiler/profilers/numerical_column_stats.py
@@ -250,7 +250,7 @@ class NumericStatsMixin(with_metaclass(abc.ABCMeta, object)):
     def kurtosis(self):
         return self._correct_bias_kurtosis(
             self.match_count,
-            self._biased_skewness)
+            self._biased_kurtosis)
 
     def _update_variance(self, batch_mean, batch_var, batch_count):
         """
@@ -314,11 +314,15 @@ class NumericStatsMixin(with_metaclass(abc.ABCMeta, object)):
             return biased_skewness1
 
         delta = mean2 - mean1
+        N = match_count1 + match_count2
         M2_1 = (match_count1 - 1) * variance1
         M2_2 = (match_count2 - 1) * variance2
+        M2 = M2_1 + M2_2 + delta**2 * match_count1 * match_count2 / N
+        if not M2:
+            return 0.0
+
         M3_1 = biased_skewness1 * np.sqrt(M2_1**3) / np.sqrt(match_count1)
         M3_2 = biased_skewness2 * np.sqrt(M2_2**3) / np.sqrt(match_count2)
-        N = match_count1 + match_count2
 
         first_term = M3_1 + M3_2
         second_term = delta**3 * match_count1 * match_count2 \
@@ -326,7 +330,6 @@ class NumericStatsMixin(with_metaclass(abc.ABCMeta, object)):
         third_term = 3 * delta * (match_count1 * M2_2
                      - match_count2 * M2_1) / N
         M3 = first_term + second_term + third_term
-        M2 = M2_1 + M2_2 + delta**2 * match_count1 * match_count2 / N
 
         biased_skewness = np.sqrt(N) * M3 / np.sqrt(M2**3)
         return biased_skewness
@@ -334,7 +337,7 @@ class NumericStatsMixin(with_metaclass(abc.ABCMeta, object)):
     @staticmethod
     def _correct_bias_skewness(match_count, biased_skewness):
         """ TODO: Documentation """
-        if match_count < 3:
+        if biased_skewness is None or np.isnan(biased_skewness) or match_count < 3:
             return np.nan
 
         skewness = np.sqrt(match_count * (match_count - 1)) \
@@ -349,18 +352,22 @@ class NumericStatsMixin(with_metaclass(abc.ABCMeta, object)):
         if np.isnan(biased_kurtosis2):
             biased_kurtosis1 = 0
         if match_count1 < 1:
-            return biased_kurtosis2;
+            return biased_kurtosis2
         elif match_count2 < 1:
             return biased_kurtosis1
 
         delta = mean2 - mean1
+        N = match_count1 + match_count2
         M2_1 = (match_count1 - 1) * variance1
         M2_2 = (match_count2 - 1) * variance2
+        M2 = M2_1 + M2_2 + delta ** 2 * match_count1 * match_count2 / N
+        if not M2:
+            return 0
+
         M3_1 = biased_skewness1 * np.sqrt(M2_1**3) / np.sqrt(match_count1)
         M3_2 = biased_skewness2 * np.sqrt(M2_2**3) / np.sqrt(match_count2)
         M4_1 = (biased_kurtosis1 + 3) * M2_1**2 / match_count1
         M4_2 = (biased_kurtosis2 + 3) * M2_2**2 / match_count2
-        N = match_count1 + match_count2
 
         first_term = M4_1 + M4_2
         second_term = delta**4 * (match_count1 * match_count2 *
@@ -371,14 +378,13 @@ class NumericStatsMixin(with_metaclass(abc.ABCMeta, object)):
         fourth_term = 4 * delta * (match_count1 * M3_2 - match_count2
                       * M3_1) / N
         M4 = first_term + second_term + third_term + fourth_term
-        M2 = M2_1 + M2_2 + delta ** 2 * match_count1 * match_count2 / N
 
         biased_kurtosis = N * M4 / M2**2 - 3
         return biased_kurtosis
 
     @staticmethod
     def _correct_bias_kurtosis(match_count, biased_kurtosis):
-        if match_count < 4:
+        if biased_kurtosis is None or np.isnan(biased_kurtosis) or match_count < 4:
             return np.nan
 
         kurtosis = (match_count - 1) / ((match_count - 2) *
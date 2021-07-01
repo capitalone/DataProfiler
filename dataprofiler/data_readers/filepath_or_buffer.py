34cacda6a6bc5c991a686a89cc8f9eb6b83453b7
Gautom Das <gautomdas4@gmail.com>
2021-07-01 14:21:10 -0400

    Context manager and test changes

--- dataprofiler/data_readers/filepath_or_buffer.py
+++ dataprofiler/data_readers/filepath_or_buffer.py
@@ -1,45 +1,44 @@
 from io import open, StringIO, BytesIO
 
-
 from . import data_utils
 
 class FileOrBufferHandler():
     """
     FileOrBufferHandler class to read a filepath or buffer in and always return a readable buffer
     """
-    def __init__(self, filepath_or_buffer, open_method='r', seek_offset=None, seek_whence=0):
+    def __init__(self, io_obj, open_method='r', seek_offset=None, seek_whence=0):
         """
         Context manager class used for inputing a file or buffer and returning a structure
         that is always a buffer.
 
-        :param filepath_or_buffer: path to the file being loaded or buffer
-        :type filepath_or_buffer: string, StringIO, or BytesIO 
+        :param io_obj: path to the file being loaded or buffer
+        :type io_obj: string, StringIO, or BytesIO 
         :param open_method: value describes the mode the file is opened in
         :type open_method: string
         :param seek_offset: offset from start of the stream
         :type seek_offset: int
         :return: TextIOBase or BufferedIOBase class/subclass
         """
-        self._filepath_or_buffer = filepath_or_buffer
+        self._io_obj = io_obj
         self.open_method = open_method
         self.seek_offset = seek_offset
         self.seek_whence = seek_whence
-        self._file = None
 
     def __enter__(self):
-        if not data_utils.is_stream_buffer(self._filepath_or_buffer):
-            self._filepath_or_buffer = open(self._filepath_or_buffer, self.open_method)
+        if isinstance(self._io_obj, str):
+            self._io_obj = open(self._io_obj, self.open_method)
+
+        elif not isinstance(self._io_obj, (StringIO, BytesIO)):
+            # Raise AttributeError if attribute value not found.
+            raise AttributeError(f'Type {type(self._io_obj)} is invalid.')
 
         if self.seek_offset is not None:
-            self._filepath_or_buffer.seek(self.seek_offset, self.seek_whence)
-            return self._filepath_or_buffer
-        else:
-            return self._filepath_or_buffer
+            self._io_obj.seek(self.seek_offset, self.seek_whence)
 
+        return self._io_obj
 
     def __exit__(self, exc_type, exc_value, exc_traceback):
-        if (self._file is None):
-            self._filepath_or_buffer.seek(0)
-            self._filepath_or_buffer.close()
+        if isinstance(self._io_obj, (StringIO, BytesIO)):
+            self._io_obj.seek(0)
         else:
-            self._file.close()
+            self._io_obj.close()
--- dataprofiler/tests/data_readers/test_filepath_or_buffer.py
+++ dataprofiler/tests/data_readers/test_filepath_or_buffer.py
@@ -2,30 +2,25 @@ import unittest
 import os
 from io import open, StringIO, BytesIO
 
-import pandas as pd
-
 from dataprofiler.data_readers.filepath_or_buffer import FileOrBufferHandler
 
 
 MODULE_PATH = os.path.dirname(os.path.abspath(__file__))
 test_root_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
+
 class TestFilepathOrBuffer(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        
+
         test_dir = os.path.join(test_root_path, 'data')
         cls.input_file_names = [
-            dict(path=os.path.join(test_dir, 'csv/diamonds.csv'),
-                 count=53940, delimiter=',', has_header=[0],
-                 num_columns=10, encoding='utf-8'),
-            dict(path=os.path.join(test_dir, 'csv/iris.csv'),
-                 count=150, delimiter=',', has_header=[0],
-                 num_columns=6, encoding='utf-8'),  
-            dict(path=os.path.join(test_dir, 'json/iris-utf-8.json'), encoding='utf-8', count=150),
-            dict(path=os.path.join(test_dir, 'txt/discussion_hn.txt'), count=10000),
-            dict(path=os.path.join(test_dir, 'txt/discussion_reddit.txt'), count=10000),   
+            dict(path=os.path.join(test_dir, 'csv/diamonds.csv')),
+            dict(path=os.path.join(test_dir, 'csv/iris.csv')),
+            dict(path=os.path.join(test_dir, 'json/iris-utf-8.json')),
+            dict(path=os.path.join(test_dir, 'txt/discussion_hn.txt')),
+            dict(path=os.path.join(test_dir, 'txt/discussion_reddit.txt')),
         ]
         cls.output_file_path = None
 
@@ -34,90 +29,82 @@ class TestFilepathOrBuffer(unittest.TestCase):
         Make sure FileOrBufferHandler can input a file and read it similarly to open()
         """
         for input_file in self.input_file_names:
-            with FileOrBufferHandler(input_file['path'], 'r') as filepath_or_buffer, open(input_file['path'], 'r') as input_file_check:
-                # check every 10th line for the first 100 lines
-                for i in range(0, 10):    
-                    for i in range(0, 9):
-                        filepath_or_buffer.readline()
-                        input_file_check.readline()
-                    self.assertEqual(filepath_or_buffer.readline(), input_file_check.readline())
+            with FileOrBufferHandler(input_file['path'], 'r') as\
+                    filepath_or_buffer, open(input_file['path'], 'r') as\
+                    input_file_check:
+
+                # check first 100 lines
+                for i in range(0, 100):
+                    self.assertEqual(filepath_or_buffer.readline(),
+                                     input_file_check.readline())
 
             # check that file was properly closed
-            self.assertEqual(filepath_or_buffer.closed, input_file_check.closed)
+            self.assertEqual(filepath_or_buffer.closed,
+                             input_file_check.closed)
 
     def test_pass_in_StringIO_buffer(self):
         """
         Make sure FileOrBufferHandler can take StringIO and read it similarly to open() 
         """
         for input_file in self.input_file_names:
-            with FileOrBufferHandler(StringIO(open(input_file['path'], 'r').read())) as filepath_or_buffer, open(input_file['path'], 'r') as input_file_check:
-                # check every 10th line for the first 100 lines
-                for i in range(0, 10):    
-                    for i in range(0, 9):
-                        filepath_or_buffer.readline()
-                        input_file_check.readline()
+            with FileOrBufferHandler(StringIO(open(input_file['path'], 'r')
+                                              .read())) as filepath_or_buffer, open(input_file['path'], 'r')\
+                    as input_file_check:
 
-                    self.assertEqual(filepath_or_buffer.readline(), input_file_check.readline())
-                    
-            # check that file was properly closed
-            self.assertEqual(filepath_or_buffer.closed, input_file_check.closed)
+                # check first 100 lines
+                for i in range(0, 100):
+                    self.assertEqual(filepath_or_buffer.readline(),
+                                     input_file_check.readline())
 
     def test_pass_in_StringIO_seek_buffer(self):
         """
         Make sure FileOrBufferHandler can take StringIO with seek and read it similarly to open() with seek
         """
         for input_file in self.input_file_names:
+            print(input_file)
             seek_offset_test = 100
-            with FileOrBufferHandler(StringIO(open(input_file['path'], 'r').read()), seek_offset=seek_offset_test) as filepath_or_buffer, open(input_file['path'], 'r') as input_file_check:
+            with FileOrBufferHandler(StringIO(open(input_file['path'], 'rb')
+                                              .read().decode()), seek_offset=seek_offset_test) as filepath_or_buffer,\
+                    open(input_file['path'], 'rb') as input_file_check:
+
                 input_file_check.seek(seek_offset_test)
 
-                # check every 10th line for the first 100 lines
-                for i in range(0, 10):    
-                    for i in range(0, 9):
-                        filepath_or_buffer.readline()
-                        input_file_check.readline()
-
-                    self.assertEqual(filepath_or_buffer.readline(), input_file_check.readline())
-                    
-            # check that file was properly closed
-            self.assertEqual(filepath_or_buffer.closed, input_file_check.closed)
+                # check first 100 lines
+                for i in range(0, 100):
+                    self.assertEqual(filepath_or_buffer.readline(),
+                                     input_file_check.readline().decode())
 
     def test_pass_in_BytesIO_buffer(self):
         """
         Make sure FileOrBufferHandler can take BytesIO and read it similarly to open() 
         """
         for input_file in self.input_file_names:
-            with FileOrBufferHandler(BytesIO(open(input_file['path'], 'rb').read())) as filepath_or_buffer, open(input_file['path'], 'rb') as input_file_check:
-                # check every 10th line for the first 100 lines
-                for i in range(0, 10):    
-                    for i in range(0, 9):
-                        filepath_or_buffer.readline()
-                        input_file_check.readline()
+            with FileOrBufferHandler(BytesIO(open(input_file['path'], 'rb').
+                                             read())) as filepath_or_buffer, open(input_file['path'], 'rb')\
+                    as input_file_check:
 
-                    self.assertEqual(filepath_or_buffer.readline(), input_file_check.readline())
-                    
-            # check that file was properly closed
-            self.assertEqual(filepath_or_buffer.closed, input_file_check.closed)
+                # check first 100 lines
+                for i in range(0, 100):
+                    self.assertEqual(filepath_or_buffer.readline(),
+                                     input_file_check.readline())
 
     def test_pass_in_BytesIO_seek_buffer(self):
         """
         Make sure FileOrBufferHandler can take BytesIO with seek and read it similarly to open() with seek
         """
         for input_file in self.input_file_names:
-            seek_offset_test = 100
-            with FileOrBufferHandler(BytesIO(open(input_file['path'], 'rb').read()), seek_offset=seek_offset_test) as filepath_or_buffer, open(input_file['path'], 'rb') as input_file_check:
-                input_file_check.seek(seek_offset_test)
-                
-                # check every 10th line for the first 100 lines
-                for i in range(0, 10):    
-                    for i in range(0, 9):
-                        filepath_or_buffer.readline()
-                        input_file_check.readline()
+            seek_offset_test = 500
+            with FileOrBufferHandler(BytesIO(open(input_file['path'], 'rb').
+                                             read()), seek_offset=seek_offset_test) as filepath_or_buffer,\
+                    open(input_file['path'], 'rb') as input_file_check:
 
-                    self.assertEqual(filepath_or_buffer.readline(), input_file_check.readline())
-                    
-            # check that file was properly closed
-            self.assertEqual(filepath_or_buffer.closed, input_file_check.closed)
+                input_file_check.seek(seek_offset_test)
+
+                # check first 100 lines
+                for i in range(0, 100):
+                    self.assertEqual(filepath_or_buffer.readline(),
+                                     input_file_check.readline())
+
 
 if __name__ == '__main__':
     unittest.main()

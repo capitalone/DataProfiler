187a3fff75c0b504273088e2317678979ce095a3
Grant <56846128+gme5078@users.noreply.github.com>
2021-07-19 14:12:19 -0700

    Unstructured Text Diff (#339)
    * Time to discuss
    
    * Clean up

--- dataprofiler/profilers/utils.py
+++ dataprofiler/profilers/utils.py
@@ -493,6 +493,52 @@ def find_diff_of_dicts(dict1, dict2):
     return diff
 
 
+def find_diff_of_dicts_with_diff_keys(dict1, dict2):
+    """
+    Finds the difference between two dicts. For each key in each dict,
+    returns "unchanged" if there's no difference, otherwise returns
+    the difference. Assumes that if the two dictionaries share the
+    same key, their values are the same type.
+
+    :param dict1: the first dict
+    :type dict1: dict
+    :param dict2: the second dict
+    :type dict2: dict
+    :return: Difference in the keys of each dict
+    :rtype: list
+    """
+
+    diff_1 = {}
+    diff_shared = {}
+    diff_2 = {}
+    for key, value1 in dict1.items():
+        if key in dict2:
+            value2 = dict2[key]
+            if isinstance(value1, list):
+                diff_shared[key] = find_diff_of_lists_and_sets(value1, value2)
+            elif isinstance(value1, datetime.datetime):
+                diff_shared[key] = find_diff_of_dates(value1, value2)
+            elif isinstance(value1, str) or isinstance(value1, bool):
+                diff_shared[key] = find_diff_of_strings_and_bools(value1, value2)
+            else:
+                diff_shared[key] = find_diff_of_numbers(value1, value2)
+        else:
+            diff_1[key] = value1
+
+    # Add any keys in dict2 that weren't in dict1
+    for key, value in dict2.items():
+        if key not in dict1:
+            diff_2[key] = value
+
+    diff = [diff_1, diff_shared, diff_2]
+
+    # If both dicts have no keys, it is unchanged
+    if diff == [{}, {}, {}]:
+        diff = "unchanged"
+
+    return diff
+
+
 def get_memory_size(data, unit='M'):
     """
     Get memory size of the input data
@@ -543,4 +589,3 @@ def method_timeit(method=None, name=None):
     if callable(method):
         return decorator(method, name_dec=name)
     return decorator
-

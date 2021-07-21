9f23fa3021f1af326e4c40e420e2d0d7404446e6
Andrew Yin <andrewyin1@gmail.com>
2021-07-21 15:56:15 -0500

    Add t-test to numerical stats diff

--- dataprofiler/profilers/numerical_column_stats.py
+++ dataprofiler/profilers/numerical_column_stats.py
@@ -294,6 +294,9 @@ class NumericStatsMixin(with_metaclass(abc.ABCMeta, object)):
                                                    other_profile.variance),
             "stddev": utils.find_diff_of_numbers(self.stddev,
                                                  other_profile.stddev),
+            "t-test": self._perform_t_test(self.mean, self.variance, self.match_count,
+                                           other_profile.mean, other_profile.variance,
+                                           other_profile.match_count)
         }
         return differences
 
@@ -329,6 +332,53 @@ class NumericStatsMixin(with_metaclass(abc.ABCMeta, object)):
             else self._correct_bias_kurtosis(
                     self.match_count,
                     self._biased_kurtosis)
+
+    @staticmethod
+    def _perform_t_test(mean1, var1, n1,
+                        mean2, var2, n2):
+        results = {}
+        s_delta = var1/n1 + var2/n2
+        t = (mean1 - mean2) / np.sqrt(s_delta)
+        conservative_df = min(n1, n2) - 1
+        welch_df = s_delta ** 2 / ((var1 / n1) ** 2 / (n1 - 1) + (var2 / n2) ** 2 / (n2 - 1))
+        results.update({
+            "t-statistic": t,
+            "conservative_df": conservative_df,
+            "welch_df": welch_df
+        })
+        try:
+            import scipy.stats
+        except ImportError:
+            # Failed, so we return the stats but don't perform the test
+            warnings.warn("Could not import necessary statistical packages. "
+                          "T-test will be incomplete.", RuntimeWarning)
+            return results
+        # If scipy import was successful, now perform the *two-sided* t-test
+        conservative_t = scipy.stats.t(conservative_df)
+        conservative_p_val = (1 - conservative_t.cdf(abs(t))) * 2
+        welch_t = scipy.stats.t(welch_df)
+        welch_p_val = (1 - welch_t.cdf(abs(t))) * 2
+        results.update({
+            'results': {
+                "conservative": {
+                    'p-value': conservative_p_val
+                },
+                "welch": {
+                    'p-value': welch_p_val
+                }
+            }
+        })
+
+        alphas = [0.01, 0.05, 0.1]
+        for alpha in alphas:
+            # If the p-val is below alpha, reject
+            conservative_result = "Reject" if conservative_p_val < alpha else "Accept"
+            welch_result = "Reject" if welch_p_val < alpha else "Accept"
+
+            results['results']["conservative"][alpha] = conservative_result
+            results['results']["welch"][alpha] = welch_result
+
+        return results
 
     def _update_variance(self, batch_mean, batch_var, batch_count):
         """

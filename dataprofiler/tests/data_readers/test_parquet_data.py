a9935bb71f4c01f12a927344da5a740cc776fc09
JGSweets <JGSweets@users.noreply.github.com>
2021-07-21 10:31:30 -0500

    Buffer issue and cleanup tests pep8 (#351)

--- dataprofiler/tests/data_readers/test_parquet_data.py
+++ dataprofiler/tests/data_readers/test_parquet_data.py
@@ -44,8 +44,8 @@ class TestParquetDataClass(unittest.TestCase):
         cls.output_file_path = None
 
     @classmethod
-    def setUp(self):
-        for buffer in self.buffer_list:
+    def setUp(cls):
+        for buffer in cls.buffer_list:
             buffer['path'].seek(0)
 
     def test_is_match_for_byte_streams(self):

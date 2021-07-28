7150be169f75016ea9693395989ce1ba74940834
az85252 <albertz4@illinois.edu>
2021-07-28 18:06:41 -0500

    created graph histograms ability and test cases

---
+++ dataprofiler/reports/graphs.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+import math
+import warnings
+
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+
+def plot_histograms(profiler, columns=None):
+    """
+        Take a input of StructuredProfiler class and a list of specified column
+        names and then plots the histograms of those that are int or float
+        columns.
+
+        :param profiler: StructuredProfiler variable
+        :param columns: list of column names to be plotted
+        :type profiler: StructuredProfiler
+        :type columns: list
+        :return:
+    """
+
+    # get all inds to graph, raise error if user specified doesn't exist (part 1)
+    inds_to_graph = []
+    if not columns:
+        inds_to_graph = list(range(len(profiler.profile)))
+    else:
+        for column in columns:
+            col = column
+            if isinstance(col, str):
+                col = col.lower()
+            if col not in profiler._col_name_to_idx:
+                raise ValueError("Column \"" + str(col) + "\" is not found as a "
+                                                       "profiler column")
+            inds_to_graph.extend(profiler._col_name_to_idx[col])
+        sorted(inds_to_graph)
+    # get all columns which are either int or float (part2)
+    for col_ind in reversed(inds_to_graph):
+        # get data_type
+        data_compiler = profiler.profile[col_ind].profiles['data_type_profile']
+        data_type = data_compiler.selected_data_type
+        data_type_profiler = data_compiler._profiles[data_type]
+        if data_type not in ['int', 'float']:
+            inds_to_graph.pop()
+
+    if not inds_to_graph:
+        warnings.warn("No plots were constructed"
+                      " because no int or float columns were found in columns")
+        return
+    # get proper tile format for graph
+    n = len(inds_to_graph)
+    cols = math.ceil(math.sqrt(n))
+    rows = math.ceil(n / cols)
+    fig, axs = plt.subplots(rows, cols)  # this will need to be flattened into a list
+    # flatten axes for inputing graphs into the plot
+    if not isinstance(axs, (np.ndarray)):
+        axs = np.array([axs])
+    axs.flatten()
+
+    # graph the plots (part 3)
+    for col_ind, ax in zip(inds_to_graph, axs):
+        col_profiler = profiler.profile[col_ind]
+        data_compiler = col_profiler.profiles['data_type_profile']
+        data_type = data_compiler.selected_data_type
+        data_type_profiler = data_compiler._profiles[data_type]
+        plot_col_histogram(data_type_profiler, ax=ax,
+                           title=str(data_type_profiler.name))
+    plt.show()
+
+    return fig
+
+
+def plot_col_histogram(data_type_profiler, ax=None, title=""):
+    histogram = data_type_profiler._get_best_histogram_for_profile()
+    plot = sns.histplot(x=histogram['bin_edges'][:-1], bins=histogram['bin_edges'],
+                 weights=histogram['bin_counts'], ax=ax)
+    plot.set_title(title)
+
---
+++ dataprofiler/tests/reports/test_graphs.py
@@ -0,0 +1,79 @@
+import unittest
+import warnings
+
+from dataprofiler.profilers import IntColumn
+from dataprofiler.reports import graphs
+from unittest import mock
+import dataprofiler as dp
+import pandas as pd
+
+@mock.patch("dataprofiler.reports.graphs.plt.show")
+@mock.patch("dataprofiler.reports.graphs.sns")
+class TestPlotHistograms(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.data = [[1, 1.0, 'a', '1/2/2021'],
+                [None, None, 'b', '1/2/2020'],
+                [3, 3.5, 'c', '1/2/2022'],
+                [4, 4.5, 'd', '1/2/2023'],
+                [5, 6.0, 'e', '5/2/2020'],
+                [None, None, 'f', '1/5/2020'],
+                [1, 1.0, 'g', '2/5/2020'],
+                [None, 10.0, 1, '3/5/2020']]
+        cls.options = dp.ProfilerOptions()
+        cls.options.set({"data_labeler.is_enabled": False})
+        cls.profiler = dp.StructuredProfiler(cls.data, options=cls.options)
+
+    @mock.patch("dataprofiler.reports.graphs.plot_col_histogram")
+    def test_no_columns_specified(self, plot_col_mock, seaborn_mock, plt_mock):
+        x = graphs.plot_histograms(self.profiler)
+        print(graphs)
+        self.assertEqual(2, plot_col_mock.call_count)
+        self.assertEqual(0, plot_col_mock.call_args_list[0].args[0].name)
+        self.assertEqual(1, plot_col_mock.call_args_list[1].args[0].name)
+        self.assertIsNotNone(x)
+
+    @mock.patch("dataprofiler.reports.graphs.plot_col_histogram")
+    def test_normal(self, plot_col_mock, seaborn_mock, plt_mock):
+        x = graphs.plot_histograms(self.profiler, [1])
+        print(graphs)
+        self.assertEqual(1, plot_col_mock.call_count)
+        self.assertEqual(1, plot_col_mock.call_args_list[0].args[0].name)
+        self.assertIsNotNone(x)
+
+    def test_bad_column_name(self, seaborn_mock, plt_mock):
+        with self.assertRaisesRegex(ValueError, "Column \"a\" is not found as a profiler column"):
+            graphs.plot_histograms(self.profiler, [0, "a"])
+
+    def test_no_column_plottable(self, seaborn_mock, plt_mock):
+        with self.assertWarnsRegex(Warning, "No plots were constructed"
+                      " because no int or float columns were found in columns"):
+            graphs.plot_histograms(self.profiler, [2, 3])
+
+    @mock.patch("dataprofiler.reports.graphs.plot_col_histogram")
+    def test_empty_profiler(self, plot_col_mock, seaborn_mock, plt_mock):
+        with self.assertWarnsRegex(Warning, "No plots were constructed"
+                      " because no int or float columns were found in columns"):
+            graphs.plot_histograms(dp.StructuredProfiler(data=None, options=self.options))
+
+
+@mock.patch("dataprofiler.reports.graphs.plt.show")
+class TestPlotColHistogram(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.data = pd.Series([1,2,4,2,5,35,32], dtype=str)
+        cls.profiler = IntColumn(cls.data.name)
+        cls.profiler.update(cls.data)
+
+    def test_normal(self, plt_mock):
+        graphs.plot_col_histogram(self.profiler)
+
+    def test_empty_data(self, plt_mock):
+        data = pd.Series([], dtype=str)
+        profiler = IntColumn(data.name)
+        with self.assertRaisesRegex(TypeError,
+                                    "'NoneType' object is not subscriptable"):
+            graphs.plot_col_histogram(profiler)
+

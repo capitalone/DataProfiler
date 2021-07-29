ae6784a9cb7cd7b98ea54ba811f9d22aad7d7c7d
az85252 <albertz4@illinois.edu>
2021-07-29 10:41:44 -0500

    updated setup file, added package requirements for graphs

--- setup.py
+++ setup.py
@@ -27,6 +27,10 @@ with open(path.join(here, 'requirements.txt'), encoding='utf-8') as f:
 # Get the install_requirements from requirements.txt
 with open(path.join(here, 'requirements-ml.txt'), encoding='utf-8') as f:
     ml_packages = f.read().splitlines()
+
+# Get the install_requirements from requirements.txt
+with open(path.join(here, 'requirements-reports.txt'), encoding='utf-8') as f:
+    reports_packages = f.read().splitlines()
     
 resource_dir = 'resources/'
 default_labeler_files = [(d, [os.path.join(d, f) for f in files])
@@ -98,7 +102,8 @@ setup(
 
     # List of run-time dependencies for the labeler. These will be installed
     # by pip when someone installs the project[<label>].
-    extras_require={ 'ml': ml_packages },
+    extras_require={ 'ml': ml_packages,
+                     'reports': reports_packages},
 
     # # If there are data files included in your packages that need to be
     # # installed, specify them here.  If using Python 2.6 or less, then these

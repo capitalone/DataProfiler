4192447b6fa731d9b8d5c1dc461679809b80fa22
Andrew Yin <andrewyin1@gmail.com>
2021-08-17 10:22:17 -0500

    Update tests

--- dataprofiler/tests/profilers/test_profile_builder.py
+++ dataprofiler/tests/profilers/test_profile_builder.py
@@ -1122,7 +1122,9 @@ class TestStructuredProfiler(unittest.TestCase):
         # Test valid input
         profile = dp.StructuredProfiler(empty_df, min_true_samples=10)
         self.assertEqual(10, profile._min_true_samples)
-        
+
+    @mock.patch('dataprofiler.profilers.profile_builder.'
+                'StructuredProfiler._update_chi2')
     def test_save_and_load(self):
         datapth = "dataprofiler/tests/data/"
         test_files = ["csv/guns.csv", "csv/iris.csv"]
@@ -2937,6 +2939,8 @@ class TestProfilerFactoryClass(unittest.TestCase):
         data_str = 'test'
         self.assertIsInstance(Profiler(data_str), UnstructuredProfiler)
 
+    @mock.patch('dataprofiler.profilers.profile_builder.'
+                'StructuredProfiler._update_chi2')
     def test_save_and_load_structured(self):
         datapth = "dataprofiler/tests/data/"
         test_files = ["csv/guns.csv", "csv/iris.csv"]

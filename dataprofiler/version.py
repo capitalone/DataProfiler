29cf0e89057542a3e4944347e396f465158e8732
JGSweets <JGSweets@users.noreply.github.com>
2021-11-19 09:45:28 -0600

    Add test for numpy removing trailing nulls from strings and Increment Version 0.7.4 (#429)
    * fix: add null string ending test
    
    * fix: increment version - 0.7.4

--- dataprofiler/version.py
+++ dataprofiler/version.py
@@ -4,7 +4,7 @@ File containers the version number for the package
 
 MAJOR               = 0
 MINOR               = 7
-MICRO               = 3
+MICRO               = 4
 
 VERSION             = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 

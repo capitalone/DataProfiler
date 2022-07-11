3feb32d5cb2006a4644fa601f508725c9232917e
MisterPNP <pnp@jgp.net>
2022-07-11 10:15:53 -0400

    cleanup test file

--- dataprofiler/tests/data_readers/test_csv_graph_data.py
+++ dataprofiler/tests/data_readers/test_csv_graph_data.py
@@ -2,6 +2,8 @@ import os
 import unittest
 from io import BytesIO, StringIO, TextIOWrapper
 
+import networkx as nx
+
 from dataprofiler.data_readers.data_utils import is_stream_buffer
 from dataprofiler.data_readers.graph_data import GraphData
 
@@ -15,128 +17,68 @@ class TestGraphDataClass(unittest.TestCase):
         
         test_dir = os.path.join(test_root_path, 'data')
         cls.input_file_names = [
-            dict(path=os.path.join(test_dir, 'csv/graph-differentiator-input-positive.csv'),
-                 count=7, delimiter=',', has_header=[0],
-                 num_columns=4, encoding='utf-8'),
-            dict(path=os.path.join(test_dir, 'csv/graph-differentiator-input-standard-positive.csv'),
-                 count=7, delimiter=',', has_header=[0],
-                 num_columns=11, encoding='utf-8'),
-            dict(path=os.path.join(test_dir, 'csv/graph-differentiator-input-negative.csv'),
-                 count=4, delimiter=',', has_header=[0],
-                 num_columns=4, encoding='utf-8'),
-            dict(path=os.path.join(test_dir, 'csv/graph-data-input-json.json'),
-                 count=4, delimiter=',', has_header=[0],
-                 num_columns=4, encoding='utf-8')
+            dict(path=os.path.join(test_dir, 'csv/graph-differentiator-input-positive.csv')),
+            dict(path=os.path.join(test_dir, 'csv/graph-differentiator-input-standard-positive.csv')),
+            dict(path=os.path.join(test_dir, 'csv/graph-differentiator-input-negative.csv')),
+            dict(path=os.path.join(test_dir, 'csv/graph-data-input-json.json')),
         ]
 
-        cls.buffer_list = []
-        for input_file in cls.input_file_names:
-            # add StringIO
-            buffer_info = input_file.copy()
-            with open(input_file['path'], 'r', encoding=input_file['encoding']) as fp:
-                buffer_info['path'] = StringIO(fp.read())
-            cls.buffer_list.append(buffer_info)
-            
-            # add BytesIO
-            buffer_info = input_file.copy()
-            with open(input_file['path'], 'rb') as fp:
-                buffer_info['path'] = BytesIO(fp.read())
-            cls.buffer_list.append(buffer_info)
-
-        cls.file_or_buf_list = cls.input_file_names + cls.buffer_list
-
-        cls.output_file_path = None
-
-    @classmethod
-    def setUp(cls):
-        for buffer in cls.buffer_list:
-            buffer['path'].seek(0)
-
-    # test find_target_string_in_column
-    def test_finding_string_in_column_positive_1(self):
+    def test_finding_string_in_column_positive(self):
         '''
-        Determine whether keywords can be detected with underscore before
+        Determine whether keywords can be detected with underscore before and after
         '''
-        column_names = ['node_src', 'node_dst', 'attribute1']
-        keyword_list = ['src', 'source']
-        self.assertTrue(GraphData._find_target_string_in_column(column_names, keyword_list))
-
-    def test_finding_string_in_column_positive_2(self):
-        '''
-        Determine whether keywords can be detected with underscore after
-        '''
-        column_names = ['src_node', 'dst_node', 'attribute1']
-        keyword_list = ['src', 'source']
-        self.assertTrue(GraphData._find_target_string_in_column(column_names, keyword_list))
+        column_names_after = ['node_src', 'node_dst', 'attribute1']
+        column_names_before = ['src_node', 'dst_node', 'attribute1']
+        keyword_list = ["src", "destination"]
+        self.assertEqual(GraphData._find_target_string_in_column(column_names_after, keyword_list), 0)
+        self.assertEqual(GraphData._find_target_string_in_column(column_names_before, keyword_list), 0)
 
     def test_finding_string_in_column_negative(self):
         '''
-        Determine whether the output is false when keywords are not found
+        Determine whether the output is false when keywords are not found or without substring delimiters
         '''
-        column_names = ['movie', 'audience_type', 'audience_source']
+        column_names_no_keywords = ['movie', 'audience_type', 'audience_source']
+        column_names_no_delimiter = ['flight_number', 'destination', 'price']
         keyword_list = ['dst', 'destination', 'target']
-        self.assertFalse(GraphData._find_target_string_in_column(column_names, keyword_list))
-
-    def test_finding_string_in_column_negative(self):
-        '''
-        Determine whether the output is false when keywords is present but a substring of a word without [_, ., -] delimiters
-        '''
-        column_names = ['flight_number', 'destination', 'price']
-        keyword_list = ['dst', 'destination', 'target']
-        self.assertFalse(GraphData._find_target_string_in_column(column_names, keyword_list))
+        self.assertEqual(GraphData._find_target_string_in_column(column_names_no_keywords, keyword_list), -1)
+        self.assertEqual(GraphData._find_target_string_in_column(column_names_no_delimiter, keyword_list), -1)
 
     #test csv_column_name
     def test_csv_column_names(self):
         """
         Determine if column names are fetched correctly and in the right format
         """
-        column_names = ['node_id_dst', 'node_id_src', 'attrib_id', 'attrib_type', 'edge_date', 'open_date_src', 'open_date_dst']
+        column_names = ['node_id_dst', 'node_id_src', 'attrib_id', 'attrib_type',\
+             'edge_date', 'open_date_src', 'open_date_dst']
         input_file = self.input_file_names[1]['path']
         options = {"header": True, "delimiter": ","}
-
-        self.assertEqual(GraphData().csv_column_names(input_file, options), column_names)
+        self.assertEqual(GraphData.csv_column_names(input_file, options), column_names)
         
     # test is_match for true output w/ different options
     def test_is_graph_positive_1(self):
         """
         Determine if the input CSV file can automatically be recognized as being a graph
         """
-        input_file = self.input_file_names[0]['path']
-        options = {"header": True, "delimiter": ","}
-        self.assertTrue(GraphData.is_match(input_file, options))
-
-    def test_is_graph_positive_2(self):
-        """
-        Determine if the input CSV file can automatically be recognized as being a graph w/ adjacency list option selected
-        """
-        input_file = self.input_file_names[1]['path']
-        options = {"header": True, "delimiter": ","}
-        self.assertTrue(GraphData.is_match(input_file, options))
+        input_file_1 = self.input_file_names[0]['path']
+        input_file_2 = self.input_file_names[1]['path']
+        options_1 = {"header": True, "delimiter": ","}
+        options_2 = {"header": True, "delimiter": ","}
+        self.assertTrue(GraphData.is_match(input_file_1, options_2))
+        self.assertTrue(GraphData.is_match(input_file_1, options_2))
 
     # test is_match for false output w/ different options
     def test_is_graph_negative_1(self):
         """
         Determine if the input CSV file can be automatically recognized as not being a graph w/ no options selected
         """
-        input_file = self.input_file_names[2]['path']
-        options = {"header": True, "delimiter": ","}
-        self.assertFalse(GraphData.is_match(input_file, options))
-
-    def test_is_graph_negative_2(self):
-        """
-        Determine if is_match behaves correctly w/ non-CSV input
-        """
-        input_file = self.input_file_names[3]['path']
-        options = {"header": True, "delimiter": ","}
-        self.assertFalse(GraphData.is_match(input_file, options))
-
-    def test_is_graph_negative_3(self):
-        """
-        Determine if is_match behaves correctly w/ a CSV that does not contain a header
-        """
         input_file = self.input_file_names[1]['path']
-        options = {"header": False, "delimiter": ","}
-        self.assertFalse(GraphData.is_match(input_file, options))
+        input_file_1 = self.input_file_names[2]['path']
+        input_file_2 = self.input_file_names[3]['path']
+        options_1 = {"header": False, "delimiter": ","}
+        options = {"header": True, "delimiter": ","}
+        self.assertFalse(GraphData.is_match(input_file_1, options))
+        self.assertFalse(GraphData.is_match(input_file_2, options))   
+        self.assertFalse(GraphData.is_match(input_file, options_1))
 
 if __name__ == '__main__':
     unittest.main()

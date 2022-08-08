74245c9774763e6466e1b3d6a1746a8f3029195a
MisterPNP <pnp@jgp.net>
2022-08-08 12:49:43 -0400

    fix mishandled encoding in GraphData, add  to checks in GraphData.is_match

--- dataprofiler/profilers/profile_builder.py
+++ dataprofiler/profilers/profile_builder.py
@@ -15,7 +15,7 @@ import networkx as nx
 import numpy as np
 import pandas as pd
 
-from dataprofiler.profilers.graph_profiler import GraphProfile
+from dataprofiler.profilers.graph_profiler import GraphProfiler
 
 from .. import data_readers, dp_logging
 from ..labelers.base_data_labeler import BaseDataLabeler

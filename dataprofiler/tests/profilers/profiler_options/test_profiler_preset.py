a3b66df281a982b16a85cf38f5a70699b928a691
Lovleen Kaur <sunybuffalo@LOVLEENs-MacBook-Pro.local>
2022-09-16 16:38:31 -0400

    Presets for ProfilerOptions

---
+++ dataprofiler/tests/profilers/profiler_options/test_profiler_preset.py
@@ -0,0 +1,44 @@
+import os
+import unittest
+from unittest import mock
+
+import numpy as np
+import pandas as pd
+
+from dataprofiler import Data, Profiler, ProfilerOptions
+from dataprofiler.labelers.base_data_labeler import BaseDataLabeler
+from dataprofiler.profilers.profiler_options import FloatOptions, IntOptions
+
+
+@mock.patch(
+    "dataprofiler.profilers.data_labeler_column_profile." "DataLabelerColumn.update",
+    return_value=None,
+)
+@mock.patch("dataprofiler.profilers.profile_builder.DataLabeler", spec=BaseDataLabeler)
+class TestProfilerPreset(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.data = Data(data=pd.DataFrame([1, 2]), data_type="csv")
+
+    def test_profiler_preset_complete(self, *mocks):
+        options = ProfilerOptions(presets="complete")
+        self.assertTrue(options.structured_options.correlation)
+        self.assertTrue(options.structured_options.null_replication_metrics.is_enabled)
+
+    def test_profiler_preset_data_types(self, *mocks):
+        options = ProfilerOptions(presets="data_types")
+        self.assertTrue(options.structured_options.data_labeler.is_enabled)
+        self.assertFalse(options.structured_options.correlation.is_enabled)
+        self.assertFalse(options.structured_options.null_replication_metrics.is_enabled)
+
+    def test_profiler_preset_numeric_stats_disabled(self, *mocks):
+        options = ProfilerOptions(presets="numeric_stats_disabled")
+        self.assertTrue(options.structured_options.data_labeler.is_enabled)
+        self.assertFalse(options.structured_options.int.is_numeric_stats_enabled)
+        self.assertFalse(options.structured_options.text.is_numeric_stats_enabled)
+        self.assertFalse(options.structured_options.float.is_numeric_stats_enabled)
+        self.assertFalse(options.structured_options.correlation.is_enabled)
+        self.assertFalse(options.structured_options.null_replication_metrics.is_enabled)
+        self.assertTrue(options.structured_options.category.is_enabled)
+        self.assertTrue(options.structured_options.order.is_enabled)
+

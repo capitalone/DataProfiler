01600c9c1ddf7a87bd0d0e8ce63779af2c95dd38
Tony Wu <tonywu315@gmail.com>
2022-09-27 14:48:04 -0400

    Fix bug

--- dataprofiler/profilers/graph_profiler.py
+++ dataprofiler/profilers/graph_profiler.py
@@ -227,18 +227,21 @@ class GraphProfiler(object):
     def _update_num_nodes(
         self,
         graph: nx.Graph,
-        prev_dependent_properties: Dict = {},
-        subset_properties: Dict = {},
+        prev_dependent_properties: Dict = None,
+        subset_properties: Dict = None,
     ) -> None:
         """Update num_nodes for profile."""
+        if subset_properties is None:
+            subset_properties = {}
+
         self._num_nodes = self._get_num_nodes(graph)
         subset_properties["num_nodes"] = self._num_nodes
 
     def _update_num_edges(
         self,
         graph: nx.Graph,
-        prev_dependent_properties: Dict = {},
-        subset_properties: Dict = {},
+        prev_dependent_properties: Dict = None,
+        subset_properties: Dict = None,
     ) -> None:
         """Update num_edges for profile."""
         self._num_edges = self._get_num_edges(graph)
@@ -246,10 +249,13 @@ class GraphProfiler(object):
     def _update_avg_node_degree(
         self,
         graph: nx.Graph,
-        prev_dependent_properties: Dict = {},
-        subset_properties: Dict = {},
+        prev_dependent_properties: Dict = None,
+        subset_properties: Dict = None,
     ) -> None:
         """Update avg_node_degree for profile."""
+        if subset_properties is None:
+            subset_properties = {}
+
         self._avg_node_degree = self._get_avg_node_degree(
             graph, subset_properties["num_nodes"]
         )
@@ -257,8 +263,8 @@ class GraphProfiler(object):
     def _update_global_max_comp_size(
         self,
         graph: nx.Graph,
-        prev_dependent_properties: Dict = {},
-        subset_properties: Dict = {},
+        prev_dependent_properties: Dict = None,
+        subset_properties: Dict = None,
     ) -> None:
         """Update global_max_component_size for profile."""
         self._global_max_component_size = self._get_global_max_component_size(graph)
@@ -266,43 +272,55 @@ class GraphProfiler(object):
     def _update_categorical_attributes(
         self,
         graph: nx.Graph,
-        prev_dependent_properties: Dict = {},
-        subset_properties: Dict = {},
+        prev_dependent_properties: Dict = None,
+        subset_properties: Dict = None,
     ) -> None:
         """Update categorical_attributes for profile."""
+        if subset_properties is None:
+            subset_properties = {}
+
         self._categorical_attributes = self._get_categorical_attributes(graph)
         subset_properties["categorical_attributes"] = self._categorical_attributes
 
     def _update_continuous_attributes(
         self,
         graph: nx.Graph,
-        prev_dependent_properties: Dict = {},
-        subset_properties: Dict = {},
+        prev_dependent_properties: Dict = None,
+        subset_properties: Dict = None,
     ) -> None:
         """Update continuous_attributes for profile."""
+        if subset_properties is None:
+            subset_properties = {}
+
         self._continuous_attributes = self._get_continuous_attributes(graph)
         subset_properties["continuous_attributes"] = self._continuous_attributes
 
     def _update_continuous_distribution(
         self,
         graph: nx.Graph,
-        prev_dependent_properties: Dict = {},
-        subset_properties: Dict = {},
+        prev_dependent_properties: Dict = None,
+        subset_properties: Dict = None,
     ) -> None:
         """Update continuous_distribution for profile."""
+        if subset_properties is None:
+            subset_properties = {}
+
         self._continuous_distribution = self._get_continuous_distribution(
-            graph, subset_properties["continuous_attributes"]
+            graph, subset_properties.get("continuous_attributes", [])
         )
 
     def _update_categorical_distribution(
         self,
         graph: nx.Graph,
-        prev_dependent_properties: Dict = {},
-        subset_properties: Dict = {},
+        prev_dependent_properties: Dict = None,
+        subset_properties: Dict = None,
     ) -> None:
         """Update categorical_distribution for profile."""
+        if subset_properties is None:
+            subset_properties = {}
+
         self._categorical_distribution = self._get_categorical_distribution(
-            graph, subset_properties["categorical_attributes"]
+            graph, subset_properties.get("categorical_attributes", [])
         )
 
     """

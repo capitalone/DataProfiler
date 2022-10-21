564ded07d172de3e66c1de21352f266da62c1d26
JGSweets <JGSweets@users.noreply.github.com>
2022-10-21 11:26:20 -0500

    Fix req missing for typing_extensions (#698)
    * fix: add typing_extensions
    
    * fix: update post version

--- dataprofiler/version.py
+++ dataprofiler/version.py
@@ -3,7 +3,11 @@
 MAJOR = 0
 MINOR = 8
 MICRO = 2
+POST = 1  # otherwise None
 
 VERSION = "%d.%d.%d" % (MAJOR, MINOR, MICRO)
 
-__version__ = VERSION
+_post_str = ""
+if POST:
+    _post_str = f".post{POST}"
+__version__ = VERSION + _post_str

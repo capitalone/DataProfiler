2d5307fc3cc23e57ea2852c025ddcb8e74ae6fdd
Sanketh Varamballi <sankethv03@gmail.com>
2022-11-02 18:08:13 -0400

    updated _get_data_as_records

--- dataprofiler/data_readers/structured_mixins.py
+++ dataprofiler/data_readers/structured_mixins.py
@@ -1,6 +1,6 @@
 """Contains mixin data class for loading datasets of tye SpreadSheet."""
 from logging import Logger
-from typing import Any, Dict, List, Optional, Union
+from typing import Any, Dict, List, Optional, Union, cast
 
 import pandas as pd
 
@@ -72,7 +72,7 @@ class SpreadSheetDataMixin(object):
     def _get_data_as_records(self, data: Any) -> List[str]:
         """Return data records."""
         records_per_line = min(len(data), self.SAMPLES_PER_LINE_DEFAULT)
-        data_ = [
+        data = [
             str(
                 "\n".join(data[i * records_per_line : (i + 1) * records_per_line])
                 .encode("UTF-8")
@@ -80,4 +80,4 @@ class SpreadSheetDataMixin(object):
             )
             for i in range((len(data) + records_per_line - 1) // records_per_line)
         ]
-        return data_
+        return cast(List[str], data)

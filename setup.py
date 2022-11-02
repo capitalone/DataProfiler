932c81790eed893b3ee4be169b3d28c4293b0b80
Taylor Turner <taylorfturner@gmail.com>
2022-11-02 16:13:58 -0400

    test

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     # packages=find_packages(exclude=['src/test', 'src/sample']),
-    packages=find_packages(exclude=["tests", "examples"]),
+    packages=find_packages(exclude=["tests/*", "examples/*"]),
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:

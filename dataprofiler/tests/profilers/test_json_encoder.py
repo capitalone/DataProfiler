99893706a8fed186aab1f12d4c12bd8c8718e2d5
kshitijavis <kshitij.avi.s@gmail.com>
2023-03-07 13:07:18 -0600

    Test ProfileEncoder on CategoricalColumn

--- dataprofiler/tests/profilers/test_json_encoder.py
+++ dataprofiler/tests/profilers/test_json_encoder.py
@@ -4,8 +4,10 @@ from collections import defaultdict
 from unittest.mock import patch
 
 import numpy as np
+import pandas as pd
 
 from dataprofiler.profilers.base_column_profilers import BaseColumnProfiler
+from dataprofiler.profilers.categorical_column_profile import CategoricalColumn
 from dataprofiler.profilers.json_encoder import ProfileEncoder
 
 
@@ -29,3 +31,60 @@ class TestJsonEncoder(unittest.TestCase):
         )
 
         self.assertEqual(json.loads(serialized), exepcted)
+
+    def test_encode_categorical_column_profiler(self):
+        profile = CategoricalColumn("0")
+
+        serialized = json.dumps(profile, cls=ProfileEncoder)
+        expected = json.dumps(
+            {
+                "name": "0",
+                "col_index": np.nan,
+                "sample_size": 0,
+                "metadata": dict(),
+                "times": defaultdict(),
+                "thread_safe": True,
+                "_categories": defaultdict(int),
+                "_CategoricalColumn__calculations": dict(),
+                "_top_k_categories": None,
+            }
+        )
+
+        self.assertEqual(json.loads(serialized), json.loads(expected))
+
+    def test_encode_categorical_column_profiler_after_update(self):
+        df_categorical = pd.Series(
+            [
+                "a",
+                "a",
+                "a",
+                "b",
+                "b",
+                "b",
+                "b",
+                "c",
+                "c",
+                "c",
+                "c",
+                "c",
+            ]
+        )
+        profile = CategoricalColumn(df_categorical.name)
+        profile.update(df_categorical)
+
+        serialized = json.dumps(profile, cls=ProfileEncoder)
+        exepcted = json.dumps(
+            {
+                "name": profile.name,
+                "col_index": profile.col_index,
+                "sample_size": profile.sample_size,
+                "metadata": profile.metadata,
+                "times": profile.times,
+                "thread_safe": profile.thread_safe,
+                "_categories": profile._categories,
+                "_CategoricalColumn__calculations": {},
+                "_top_k_categories": profile._top_k_categories,
+            }
+        )
+
+        self.assertEqual(json.loads(serialized), json.loads(exepcted))

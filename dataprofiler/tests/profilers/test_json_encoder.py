39352539dbcfa2349f078bd8527498eb1d6a2a91
kshitijavis <kshitij.avi.s@gmail.com>
2023-03-16 18:51:48 -0500

    Test ProfilerEncoder on OrderColumn

--- dataprofiler/tests/profilers/test_json_encoder.py
+++ dataprofiler/tests/profilers/test_json_encoder.py
@@ -9,6 +9,7 @@ import pandas as pd
 from dataprofiler.profilers.base_column_profilers import BaseColumnProfiler
 from dataprofiler.profilers.categorical_column_profile import CategoricalColumn
 from dataprofiler.profilers.json_encoder import ProfileEncoder
+from dataprofiler.profilers.order_column_profile import OrderColumn
 
 
 class TestJsonEncoder(unittest.TestCase):
@@ -85,6 +86,54 @@ class TestJsonEncoder(unittest.TestCase):
                 "_CategoricalColumn__calculations": {},
                 "_top_k_categories": None,
             },
+        )
+
+        self.assertEqual(serialized, expected)
+
+    def test_encode_order_column_profiler(self):
+        profile = OrderColumn("0")
+
+        serialized = json.dumps(profile, cls=ProfileEncoder)
+        expected = json.dumps(
+            {
+                "order": None,
+                "_last_value": None,
+                "_first_value": None,
+                "_piecewise": False,
+                "_OrderColumn__calculations": dict(),
+                "name": "0",
+                "col_index": np.nan,
+                "sample_size": 0,
+                "metadata": dict(),
+                "times": defaultdict(),
+                "thread_safe": True,
+            }
+        )
+
+        self.assertEqual(serialized, expected)
+
+    def test_encode_order_column_profiler_after_update(self):
+        profile = OrderColumn("0")
+
+        df_order = pd.Series(["za", "z", "c", "a"])
+        with patch("time.time", side_effect=lambda: 0.0):
+            profile.update(df_order)
+
+        serialized = json.dumps(profile, cls=ProfileEncoder)
+        expected = json.dumps(
+            {
+                "order": "descending",
+                "_last_value": "a",
+                "_first_value": "za",
+                "_piecewise": False,
+                "_OrderColumn__calculations": dict(),
+                "name": "0",
+                "col_index": np.nan,
+                "sample_size": 4,
+                "metadata": dict(),
+                "times": {"order": 0.0},
+                "thread_safe": True,
+            }
         )
 
         self.assertEqual(serialized, expected)

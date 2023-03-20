3db583673f3c7dcd2366b622be9c20717266f98b
Kshitij Sinha <55467782+kshitijavis@users.noreply.github.com>
2023-03-20 12:56:07 -0500

    Encode categorical column (#770)
    * Test ProfileEncoder on CategoricalColumn
    
    * Fix variable name typo
    
    * Compare serialized strings without calling json.loads
    
    * Remove sort keys

--- dataprofiler/tests/profilers/test_json_encoder.py
+++ dataprofiler/tests/profilers/test_json_encoder.py
@@ -4,8 +4,10 @@ from collections import defaultdict
 from unittest.mock import patch
 
 import numpy as np
+import pandas as pd
 
 from dataprofiler.profilers.base_column_profilers import BaseColumnProfiler
+from dataprofiler.profilers.categorical_column_profile import CategoricalColumn
 from dataprofiler.profilers.json_encoder import ProfileEncoder
 
 
@@ -15,17 +17,75 @@ class TestJsonEncoder(unittest.TestCase):
             profile = BaseColumnProfiler(name="0")
 
         serialized = json.dumps(profile, cls=ProfileEncoder)
-        exepcted = json.loads(
-            json.dumps(
-                {
-                    "name": "0",
-                    "col_index": np.nan,
-                    "sample_size": 0,
-                    "metadata": dict(),
-                    "times": defaultdict(),
-                    "thread_safe": True,
-                }
-            )
+        expected = json.dumps(
+            {
+                "name": "0",
+                "col_index": np.nan,
+                "sample_size": 0,
+                "metadata": dict(),
+                "times": defaultdict(),
+                "thread_safe": True,
+            }
         )
 
-        self.assertEqual(json.loads(serialized), exepcted)
+        self.assertEqual(serialized, expected)
+
+    def test_encode_categorical_column_profiler(self):
+        profile = CategoricalColumn("0")
+
+        serialized = json.dumps(profile, cls=ProfileEncoder)
+        expected = json.dumps(
+            {
+                "name": "0",
+                "col_index": np.nan,
+                "sample_size": 0,
+                "metadata": dict(),
+                "times": defaultdict(),
+                "thread_safe": True,
+                "_categories": defaultdict(int),
+                "_CategoricalColumn__calculations": dict(),
+                "_top_k_categories": None,
+            }
+        )
+
+        self.assertEqual(serialized, expected)
+
+    def test_encode_categorical_column_profiler_after_update(self):
+        df_categorical = pd.Series(
+            [
+                "a",
+                "a",
+                "a",
+                "b",
+                "b",
+                "b",
+                "b",
+                "c",
+                "c",
+                "c",
+                "c",
+                "c",
+            ]
+        )
+        profile = CategoricalColumn(df_categorical.name)
+
+        with patch("time.time", side_effect=lambda: 0.0):
+            profile.update(df_categorical)
+
+        serialized = json.dumps(profile, cls=ProfileEncoder)
+
+        expected = json.dumps(
+            {
+                "name": None,
+                "col_index": np.nan,
+                "sample_size": 12,
+                "metadata": {},
+                "times": {"categories": 0.0},
+                "thread_safe": True,
+                "_categories": {"c": 5, "b": 4, "a": 3},
+                "_CategoricalColumn__calculations": {},
+                "_top_k_categories": None,
+            },
+        )
+
+        self.assertEqual(serialized, expected)

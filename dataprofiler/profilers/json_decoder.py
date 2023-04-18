156720e49951fa3c4886a2e3218eabfd6dea497b
kshitijavis <kshitij.avi.s@gmail.com>
2023-04-18 02:04:51 -0500

    Create method to get col profiler class by classname

--- dataprofiler/profilers/json_decoder.py
+++ dataprofiler/profilers/json_decoder.py
@@ -1,6 +1,25 @@
 """Contains methods to decode components of a Profiler."""
 
-from . import categorical_column_profile
+from .base_column_profilers import BaseColumnProfiler
+from .categorical_column_profile import CategoricalColumn
+
+
+def get_column_profiler_class(class_name: str) -> BaseColumnProfiler:
+    """
+    Use name of class to return default-constructed version of that class.
+
+    Raises ValueError if class_name is not name of a subclass of
+        BaseColumnProfiler.
+
+    :param class_name: name of BaseColumnProfiler subclass retreived by
+        calling type(instance).__name__
+    :type class_name: str representing name of class
+    :return: subclass of BaseColumnProfiler object
+    """
+    if class_name == CategoricalColumn.__name__:
+        return CategoricalColumn(None)
+    else:
+        raise ValueError(f"Invalid profiler class {class_name} " f"failed to load.")
 
 
 def decode_categorical_column(to_decode: dict):
@@ -11,6 +30,6 @@ def decode_categorical_column(to_decode: dict):
     :type to_serialize: a dictionary resullting from json.loads()
     :return: CategoricalColumn object
     """
-    decoded = categorical_column_profile.CategoricalColumn(to_decode["name"])
+    decoded = CategoricalColumn(to_decode["name"])
     for attr, value in to_decode.items():
         decoded.__setattr__(attr, value)

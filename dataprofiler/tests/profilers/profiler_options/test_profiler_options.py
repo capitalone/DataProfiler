259513db1570732c3ae1e02fb4a590c44c80c932
Richard Bann <richard@bann.com>
2023-06-13 14:54:05 -0400

    Unit test for RowStatisticOptions:

--- dataprofiler/tests/profilers/profiler_options/test_profiler_options.py
+++ dataprofiler/tests/profilers/profiler_options/test_profiler_options.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 from dataprofiler import Data, Profiler, ProfilerOptions
 from dataprofiler.labelers.base_data_labeler import BaseDataLabeler
-from dataprofiler.profilers.profiler_options import FloatOptions, IntOptions
+from dataprofiler.profilers.profiler_options import FloatOptions, IntOptions, RowStatisticsOptions
 
 
 @mock.patch(
@@ -54,6 +54,13 @@ class TestProfilerOptions(unittest.TestCase):
         profile2 = Profiler(self.data, options=options)
         # Stored in Profiler as StructuredOptions
         self.assertEqual(profile2.options, options.structured_options)
+    
+    def test_row_statistics_options(self, *mocks):
+        options = RowStatisticsOptions()
+        self.assertTrue(hasattr(options, "unique_count"))
+        self.assertTrue(hasattr(options, "null_count"))
+        self.assertTrue(options.null_count.is_enabled)
+        self.assertTrue(options.unique_count.is_enabled)
 
     def test_set_failures(self, *mocks):
 

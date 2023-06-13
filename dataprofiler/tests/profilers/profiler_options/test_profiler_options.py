3c7efed9cfcf24bc7160776d5bb87157ef80d48b
Richard Bann <richard@bann.com>
2023-06-13 16:55:04 -0400

    add line

--- dataprofiler/tests/profilers/profiler_options/test_profiler_options.py
+++ dataprofiler/tests/profilers/profiler_options/test_profiler_options.py
@@ -8,6 +8,7 @@ from dataprofiler import Data, Profiler, ProfilerOptions
 from dataprofiler.labelers.base_data_labeler import BaseDataLabeler
 from dataprofiler.profilers.profiler_options import FloatOptions, IntOptions
 
+
 @mock.patch(
     "dataprofiler.profilers.data_labeler_column_profile." "DataLabelerColumn.update",
     return_value=None,

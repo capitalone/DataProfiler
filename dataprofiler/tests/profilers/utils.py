Adds deserialization for compilers and validates tests for Primitive; fixes numerical deserialization (#886)
* feat: add test and compiler serialization

* fix: move primitive tests to own class

* feat: add primitive col compiler save tests

* fix: float serializers asserts

* feat: add tests and allow primitive compiler to deserialize

* fix: bug in numeric stats deserial

* fix: missing `)` after conflict resolution

dataprofiler/tests/profilers/utils.py
@@ -183,8 +183,9 @@ def assert_profiles_equal(actual, expected):
     actual_dict = actual.__dict__ if not isinstance(actual, dict) else actual
     expected_dict = expected.__dict__ if not isinstance(expected, dict) else expected
 
-    assert len(actual_dict.keys()) == len(expected_dict.keys())
-    assert actual_dict.keys() == expected_dict.keys()
+    assert (
+        actual_dict.keys() == expected_dict.keys()
+    ), f"{actual_dict.keys()} != {expected_dict.keys()}"
 
     for key in expected_dict.keys():
         actual_value = actual_dict.get(key, None)
@@ -201,7 +202,7 @@ def assert_profiles_equal(actual, expected):
         elif isinstance(actual_value, dict):
             assert_profiles_equal(actual_value, expected_value)
         elif isinstance(actual_value, numbers.Number):
-            np.testing.assert_equal(actual_value, expected_value)
+            np.testing.assert_equal(actual_value, expected_value, f"{key}")
         elif isinstance(actual_value, np.ndarray):
             np.testing.assert_array_equal(actual_value, expected_value)
         else:
Adds deserialization for compilers and validates tests for Primitive; fixes numerical deserialization (#886)
* feat: add test and compiler serialization

* fix: move primitive tests to own class

* feat: add primitive col compiler save tests

* fix: float serializers asserts

* feat: add tests and allow primitive compiler to deserialize

* fix: bug in numeric stats deserial

* fix: missing `)` after conflict resolution

dataprofiler/profilers/numerical_column_stats.py
@@ -468,7 +468,7 @@ class NumericStatsMixin(BaseColumnProfiler[NumericStatsMixinT], metaclass=abc.AB
         if self.num_zeros is not None:
             self.num_zeros = np.int64(self.num_zeros)
         if self.num_negatives is not None:
-            self.num_negatives = np.int64(self.num_zeros)
+            self.num_negatives = np.int64(self.num_negatives)
         if not np.isnan(self._biased_variance):
             self._biased_variance = np.float64(self._biased_variance)
         if not np.isnan(self._biased_variance):
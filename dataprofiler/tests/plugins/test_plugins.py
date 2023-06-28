44893b08e75e7b728aca59af8a09eb1493c86869
lizlouise1335 <liz.smith@richmond.edu>
2023-06-28 14:41:07 -0400

    nothing yet, just want this in two different repos

--- dataprofiler/tests/plugins/test_plugins.py
+++ dataprofiler/tests/plugins/test_plugins.py
@@ -7,17 +7,21 @@ from dataprofiler.plugins.decorators import plugin_decorator, plugins_dict
 
 
 class TestPlugins(unittest.TestCase):
-    def test_plugin_presets(self, *mocks):
+    def test_decorator_get_plugin(self, *mocks):
         mock_plugin_execution = mock.Mock()
         with mock.patch.dict(plugins_dict, defaultdict(dict)) as mock_plugin_dict:
 
-            @plugin_decorator(typ="test_preset", name="mock_test")
+            @plugin_decorator(typ="test", name="mock_test")
             def test_plugin():
                 mock_plugin_execution()
 
             expected_default_dict = defaultdict(dict)
-            expected_default_dict["test_preset"]["mock_test"] = test_plugin
+            expected_default_dict["test"]["mock_test"] = test_plugin
             self.assertDictEqual(expected_default_dict, mock_plugin_dict)
 
-            test_get_dict = get_plugins("test_preset")
+            test_get_dict = get_plugins("test")
             self.assertDictEqual({"mock_test": test_plugin}, test_get_dict)
+
+    @mock.patch("..plugins.__init__.load_plugins")
+    def test_load_plugin(self, *mocks):
+        return None

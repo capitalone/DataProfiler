a0874b437eefec2e743ca60e68bc616def8f815f
jacob-buehler <jacobbuehler@college.harvard.edu>
2023-07-13 13:31:26 -0400

    edited seed declaration statement

--- dataprofiler/data_readers/data_utils.py
+++ dataprofiler/data_readers/data_utils.py
@@ -277,8 +277,7 @@ def get_random_number_generator() -> np.random._generator.Generator:
     """Create a random number generator using a manual seed DATAPROFILER_SEED."""
     rng = np.random.default_rng(settings._seed)
     if "DATAPROFILER_SEED" in os.environ and settings._seed is None:
-        seed = os.environ.get("DATAPROFILER_SEED")
-        seed = str(seed)
+        seed: str = os.environ.get("DATAPROFILER_SEED", "")
         try:
             rng = np.random.default_rng(int(seed))
         except ValueError:

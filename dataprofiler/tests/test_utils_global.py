8c441d04737d21323ec3e8b506cc28657028bc08
jacob-buehler <jacobbuehler@college.harvard.edu>
2023-07-18 15:15:59 -0400

    removed unnecessary slicing/indexing

--- dataprofiler/tests/test_utils_global.py
+++ dataprofiler/tests/test_utils_global.py
@@ -15,14 +15,14 @@ class TestOriginalFunction(unittest.TestCase):
     def test_return_random_value(self, *mocks):
         """If DATAPROFILER_SEED not set, test that rng returns different random value."""
         rng = utils_global.get_random_number_generator()
-        sample_value = rng.integers(0, 1e6, 1)[0]
+        sample_value = rng.integers(0, 1e6)
         self.assertNotEqual(sample_value, 850624)
 
     @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=123)
     def test_return_default_rng_with_settings_seed(self):
         """If DATAPROFILER_SEED not set, test that rng uses seed = settings._seed."""
         rng = utils_global.get_random_number_generator()
-        actual_value = rng.integers(0, 100, 1)[0]
+        actual_value = rng.integers(0, 100)
         expected_value_generator = np.random.default_rng(123)
         expected_value = expected_value_generator.integers(0, 100, 1)[0]
         self.assertEqual(actual_value, expected_value)
@@ -32,7 +32,7 @@ class TestOriginalFunction(unittest.TestCase):
     def test_dataprofiler_seed_true_settings_seed_false(self):
         """Verify that we get the expected result when DATAPROFILER_SEED in os.environ and settings._seed!=None."""
         rng = utils_global.get_random_number_generator()
-        actual_value = rng.integers(0, 100, 1)[0]
+        actual_value = rng.integers(0, 100)
         expected_value_generator = np.random.default_rng(123)
         expected_value = expected_value_generator.integers(0, 100, 1)[0]
         self.assertEqual(actual_value, expected_value)
@@ -43,7 +43,7 @@ class TestOriginalFunction(unittest.TestCase):
         with unittest.mock.patch.dict("os.environ"):
             del os.environ["DATAPROFILER_SEED"]
             rng = utils_global.get_random_number_generator()
-            actual_value = rng.integers(0, 1e6, 1)[0]
+            actual_value = rng.integers(0, 1e6)
         self.assertNotEqual(actual_value, 850624)
 
     @unittest.mock.patch.dict(os.environ, {"DATAPROFILER_SEED": "123"}, clear=True)
@@ -62,9 +62,9 @@ class TestOriginalFunction(unittest.TestCase):
         with unittest.mock.patch.dict("os.environ"):
             del os.environ["DATAPROFILER_SEED"]
             rng = utils_global.get_random_number_generator()
-            actual_value = rng.integers(0, 1e6, 1)[0]
+            actual_value = rng.integers(0, 1e6)
             expected_value_generator = np.random.default_rng(123)
-            expected_value = expected_value_generator.integers(0, 1e6, 1)[0]
+            expected_value = expected_value_generator.integers(0, 1e6)
         self.assertEqual(actual_value, expected_value)
 
     @unittest.mock.patch.dict(
@@ -74,14 +74,14 @@ class TestOriginalFunction(unittest.TestCase):
     def test_warning_raised(self):
         with self.assertWarnsRegex(RuntimeWarning, "Seed should be an integer"):
             rng = utils_global.get_random_number_generator()
-            actual_value = rng.integers(0, 1e6, 1)[0]
+            actual_value = rng.integers(0, 1e6)
         self.assertNotEqual(actual_value, 850624)
 
     @unittest.mock.patch.dict(os.environ, {"DATAPROFILER_SEED": "0"}, clear=True)
     @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=123)
     def test_try_returned(self):
         rng = utils_global.get_random_number_generator()
-        actual_value = rng.integers(0, 100, 1)[0]
+        actual_value = rng.integers(0, 100)
         expected_value_generator = np.random.default_rng(123)
         expected_value = expected_value_generator.integers(0, 100, 1)[0]
         self.assertEqual(actual_value, expected_value)

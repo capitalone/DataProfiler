bb2ad99436d138358ec9f5b8f1a24517e971c132
jacob-buehler <jacobbuehler@college.harvard.edu>
2023-07-19 11:12:35 -0400

    cleaned up os.environ mocks in test_utils_global

--- dataprofiler/tests/test_utils_global.py
+++ dataprofiler/tests/test_utils_global.py
@@ -1,6 +1,7 @@
 """Validates that generator intakes DATAPROFILER_SEED properly."""
 import os
 import unittest
+import unittest.mock
 
 import numpy as np
 from numpy.random import PCG64
@@ -11,7 +12,7 @@ from .. import utils_global
 class TestOriginalFunction(unittest.TestCase):
     """Validates get_random_number_generator() is properly working."""
 
-    @unittest.mock.patch("os.environ")
+    @unittest.mock.patch.dict("os.environ", clear=True)
     def test_return_random_value(self, *mocks):
         """If DATAPROFILER_SEED not set, test that rng returns different random value."""
         rng = utils_global.get_random_number_generator()
@@ -38,12 +39,11 @@ class TestOriginalFunction(unittest.TestCase):
         self.assertEqual(actual_value, expected_value)
 
     @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=None)
+    @unittest.mock.patch.dict("os.environ", clear=True)
     def test_dataprofiler_seed_false_settings_seed_true(self):
         """Verify that we get the expected result when DATAPROFILER_SEED not in os.environ and settings._seed==None."""
-        with unittest.mock.patch.dict("os.environ"):
-            del os.environ["DATAPROFILER_SEED"]
-            rng = utils_global.get_random_number_generator()
-            actual_value = rng.integers(0, 1e6)
+        rng = utils_global.get_random_number_generator()
+        actual_value = rng.integers(0, 1e6)
         self.assertNotEqual(actual_value, 850624)
 
     @unittest.mock.patch.dict(os.environ, {"DATAPROFILER_SEED": "123"}, clear=True)
@@ -57,14 +57,13 @@ class TestOriginalFunction(unittest.TestCase):
         self.assertEqual(actual_value, expected_value)
 
     @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=123)
+    @unittest.mock.patch.dict("os.environ", clear=True)
     def test_dataprofiler_seed_false_settings_seed_false(self):
         """Verify that we get the expected result when DATAPROFILER_SEED not in os.environ and settings._seed!=None."""
-        with unittest.mock.patch.dict("os.environ"):
-            del os.environ["DATAPROFILER_SEED"]
-            rng = utils_global.get_random_number_generator()
-            actual_value = rng.integers(0, 1e6)
-            expected_value_generator = np.random.default_rng(123)
-            expected_value = expected_value_generator.integers(0, 1e6)
+        rng = utils_global.get_random_number_generator()
+        actual_value = rng.integers(0, 1e6)
+        expected_value_generator = np.random.default_rng(123)
+        expected_value = expected_value_generator.integers(0, 1e6)
         self.assertEqual(actual_value, expected_value)
 
     @unittest.mock.patch.dict(

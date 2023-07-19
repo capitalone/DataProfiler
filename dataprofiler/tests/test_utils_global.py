b657a771ed5fa61c52b47f963ea378a89b3cc242
jacob-buehler <jacobbuehler@college.harvard.edu>
2023-07-19 13:29:53 -0400

    mocked expected values in unit tests

--- dataprofiler/tests/test_utils_global.py
+++ dataprofiler/tests/test_utils_global.py
@@ -9,23 +9,31 @@ from numpy.random import PCG64
 from .. import utils_global
 
 
-class TestOriginalFunction(unittest.TestCase):
+class TestGetRandomNumberGenerator(unittest.TestCase):
     """Validates get_random_number_generator() is properly working."""
 
     @unittest.mock.patch.dict("os.environ", clear=True)
     def test_return_random_value(self, *mocks):
         """If DATAPROFILER_SEED not set, test that rng returns different random value."""
         rng = utils_global.get_random_number_generator()
-        sample_value = rng.integers(0, 1e6)
-        self.assertNotEqual(sample_value, 850624)
+        actual_value = rng.integers(0, 1e6)
+        with unittest.mock.patch(
+            "numpy.random.randint",
+        ) as numpy_mock:
+            numpy_mock.return_value = 850624
+            expected_value = np.random.randint(1)
+        self.assertNotEqual(actual_value, expected_value)
 
     @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=123)
     def test_return_default_rng_with_settings_seed(self):
         """If DATAPROFILER_SEED not set, test that rng uses seed = settings._seed."""
         rng = utils_global.get_random_number_generator()
-        actual_value = rng.integers(0, 100)
-        expected_value_generator = np.random.default_rng(123)
-        expected_value = expected_value_generator.integers(0, 100)
+        actual_value = rng.integers(0, 1e6)
+        with unittest.mock.patch(
+            "numpy.random.randint",
+        ) as numpy_mock:
+            numpy_mock.return_value = 15440
+            expected_value = np.random.randint(1)
         self.assertEqual(actual_value, expected_value)
 
     @unittest.mock.patch.dict(os.environ, {"DATAPROFILER_SEED": "0"}, clear=True)
@@ -33,9 +41,12 @@ class TestOriginalFunction(unittest.TestCase):
     def test_dataprofiler_seed_true_settings_seed_false(self):
         """Verify that we get the expected result when DATAPROFILER_SEED in os.environ and settings._seed!=None."""
         rng = utils_global.get_random_number_generator()
-        actual_value = rng.integers(0, 100)
-        expected_value_generator = np.random.default_rng(123)
-        expected_value = expected_value_generator.integers(0, 100)
+        actual_value = rng.integers(0, 1e6)
+        with unittest.mock.patch(
+            "numpy.random.randint",
+        ) as numpy_mock:
+            numpy_mock.return_value = 15440
+            expected_value = np.random.randint(1)
         self.assertEqual(actual_value, expected_value)
 
     @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=None)
@@ -44,7 +55,12 @@ class TestOriginalFunction(unittest.TestCase):
         """Verify that we get the expected result when DATAPROFILER_SEED not in os.environ and settings._seed==None."""
         rng = utils_global.get_random_number_generator()
         actual_value = rng.integers(0, 1e6)
-        self.assertNotEqual(actual_value, 850624)
+        with unittest.mock.patch(
+            "numpy.random.randint",
+        ) as numpy_mock:
+            numpy_mock.return_value = 850624
+            expected_value = np.random.randint(1)
+        self.assertNotEqual(actual_value, expected_value)
 
     @unittest.mock.patch.dict(os.environ, {"DATAPROFILER_SEED": "123"}, clear=True)
     @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=None)
@@ -52,8 +68,11 @@ class TestOriginalFunction(unittest.TestCase):
         """Verify that we get the expected result when DATAPROFILER_SEED in os.environ and settings._seed==None."""
         rng = utils_global.get_random_number_generator()
         actual_value = rng.integers(0, 1e6)
-        expected_value_generator = np.random.default_rng(123)
-        expected_value = expected_value_generator.integers(0, 1e6)
+        with unittest.mock.patch(
+            "numpy.random.randint",
+        ) as numpy_mock:
+            numpy_mock.return_value = 15440
+            expected_value = np.random.randint(1)
         self.assertEqual(actual_value, expected_value)
 
     @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=123)
@@ -62,8 +81,11 @@ class TestOriginalFunction(unittest.TestCase):
         """Verify that we get the expected result when DATAPROFILER_SEED not in os.environ and settings._seed!=None."""
         rng = utils_global.get_random_number_generator()
         actual_value = rng.integers(0, 1e6)
-        expected_value_generator = np.random.default_rng(123)
-        expected_value = expected_value_generator.integers(0, 1e6)
+        with unittest.mock.patch(
+            "numpy.random.randint",
+        ) as numpy_mock:
+            numpy_mock.return_value = 15440
+            expected_value = np.random.randint(1)
         self.assertEqual(actual_value, expected_value)
 
     @unittest.mock.patch.dict(
@@ -74,13 +96,20 @@ class TestOriginalFunction(unittest.TestCase):
         with self.assertWarnsRegex(RuntimeWarning, "Seed should be an integer"):
             rng = utils_global.get_random_number_generator()
             actual_value = rng.integers(0, 1e6)
-        self.assertNotEqual(actual_value, 850624)
+        with unittest.mock.patch(
+            "numpy.random.randint",
+        ) as numpy_mock:
+            numpy_mock.return_value = 850624
+            expected_value = np.random.randint(1)
+        self.assertNotEqual(actual_value, expected_value)
 
     @unittest.mock.patch.dict(os.environ, {"DATAPROFILER_SEED": "0"}, clear=True)
-    @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=123)
     def test_try_returned(self):
         rng = utils_global.get_random_number_generator()
         actual_value = rng.integers(0, 100)
-        expected_value_generator = np.random.default_rng(123)
-        expected_value = expected_value_generator.integers(0, 100)
+        with unittest.mock.patch(
+            "numpy.random.randint",
+        ) as numpy_mock:
+            numpy_mock.return_value = 85
+            expected_value = np.random.randint(1)
         self.assertEqual(actual_value, expected_value)

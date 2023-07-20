879d937d4fdaf9633fbda8802b3e1d9762a80438
jacob-buehler <jacobbuehler@college.harvard.edu>
2023-07-20 10:54:15 -0400

    removed unnecessary clear=True clauses; removed duplicate assert statement

--- dataprofiler/tests/test_utils_global.py
+++ dataprofiler/tests/test_utils_global.py
@@ -12,7 +12,7 @@ from .. import utils_global
 class TestGetRandomNumberGenerator(unittest.TestCase):
     """Validates get_random_number_generator() is properly working."""
 
-    @unittest.mock.patch.dict(os.environ, {"DATAPROFILER_SEED": "0"}, clear=True)
+    @unittest.mock.patch.dict(os.environ, {"DATAPROFILER_SEED": "0"})
     @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=123)
     def test_dataprofiler_seed_true_settings_seed_false(self):
         """Test for DATAPROFILER_SEED in os.environ and settings._seed!=None."""
@@ -30,7 +30,7 @@ class TestGetRandomNumberGenerator(unittest.TestCase):
             self.assertEqual(mock_np_generator.call_count, 1)
             mock_np_generator.assert_called_with(None)
 
-    @unittest.mock.patch.dict(os.environ, {"DATAPROFILER_SEED": "123"}, clear=True)
+    @unittest.mock.patch.dict(os.environ, {"DATAPROFILER_SEED": "123"})
     @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=None)
     def test_dataprofiler_seed_true_settings_seed_true(self):
         """Test for DATAPROFILER_SEED in os.environ and settings._seed==None."""
@@ -40,7 +40,7 @@ class TestGetRandomNumberGenerator(unittest.TestCase):
             mock_np_generator.assert_called_with(123)
 
     @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=123)
-    @unittest.mock.patch.dict("os.environ", clear=True)
+    @unittest.mock.patch.dict("os.environ")
     def test_dataprofiler_seed_false_settings_seed_false(self):
         """Test for DATAPROFILER_SEED not in os.environ and settings._seed!=None."""
         with unittest.mock.patch("numpy.random.default_rng") as mock_np_generator:
@@ -48,15 +48,9 @@ class TestGetRandomNumberGenerator(unittest.TestCase):
             self.assertEqual(mock_np_generator.call_count, 1)
             mock_np_generator.assert_called_with(123)
 
-    @unittest.mock.patch.dict(
-        os.environ, {"DATAPROFILER_SEED": "George Washington"}, clear=True
-    )
+    @unittest.mock.patch.dict(os.environ, {"DATAPROFILER_SEED": "George Washington"})
     @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=None)
     def test_warning_raised(self):
         """Test that warning raises if seed is not an integer."""
         with self.assertWarnsRegex(RuntimeWarning, "Seed should be an integer"):
             rng = utils_global.get_random_number_generator()
-        with unittest.mock.patch("numpy.random.default_rng") as mock_np_generator:
-            rng = utils_global.get_random_number_generator()
-            self.assertEqual(mock_np_generator.call_count, 1)
-            mock_np_generator.assert_called_with(None)

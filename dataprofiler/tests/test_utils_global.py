358d1d9edae593c094a52d61e081e41116e5c3ce
jacob-buehler <jacobbuehler@college.harvard.edu>
2023-07-20 09:39:43 -0400

    added more descriptive mock names; ensured that rng generator uses proper seed

--- dataprofiler/tests/test_utils_global.py
+++ dataprofiler/tests/test_utils_global.py
@@ -16,33 +16,45 @@ class TestGetRandomNumberGenerator(unittest.TestCase):
     @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=123)
     def test_dataprofiler_seed_true_settings_seed_false(self):
         """Verify that we get the expected result when DATAPROFILER_SEED in os.environ and settings._seed!=None."""
-        with unittest.mock.patch("numpy.random.default_rng") as mock_fnc:
+        with unittest.mock.patch(
+            "numpy.random.default_rng"
+        ) as mock_np_generator_declaration:
             rng = utils_global.get_random_number_generator()
-            self.assertEqual(mock_fnc.call_count, 1)
+            self.assertEqual(mock_np_generator_declaration.call_count, 1)
+            mock_np_generator_declaration.assert_called_with(123)
 
     @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=None)
     @unittest.mock.patch.dict("os.environ", clear=True)
     def test_dataprofiler_seed_false_settings_seed_true(self):
         """Verify that we get the expected result when DATAPROFILER_SEED not in os.environ and settings._seed==None."""
-        with unittest.mock.patch("numpy.random.default_rng") as mock_fnc:
+        with unittest.mock.patch(
+            "numpy.random.default_rng"
+        ) as mock_np_generator_declaration:
             rng = utils_global.get_random_number_generator()
-            self.assertEqual(mock_fnc.call_count, 1)
+            self.assertEqual(mock_np_generator_declaration.call_count, 1)
+            mock_np_generator_declaration.assert_called_with(None)
 
     @unittest.mock.patch.dict(os.environ, {"DATAPROFILER_SEED": "123"}, clear=True)
     @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=None)
     def test_dataprofiler_seed_true_settings_seed_true(self):
         """Verify that we get the expected result when DATAPROFILER_SEED in os.environ and settings._seed==None."""
-        with unittest.mock.patch("numpy.random.default_rng") as mock_fnc:
+        with unittest.mock.patch(
+            "numpy.random.default_rng"
+        ) as mock_np_generator_declaration:
             rng = utils_global.get_random_number_generator()
-            self.assertEqual(mock_fnc.call_count, 2)
+            self.assertEqual(mock_np_generator_declaration.call_count, 2)
+            mock_np_generator_declaration.assert_called_with(123)
 
     @unittest.mock.patch("dataprofiler.utils_global.settings._seed", new=123)
     @unittest.mock.patch.dict("os.environ", clear=True)
     def test_dataprofiler_seed_false_settings_seed_false(self):
         """Verify that we get the expected result when DATAPROFILER_SEED not in os.environ and settings._seed!=None."""
-        with unittest.mock.patch("numpy.random.default_rng") as mock_fnc:
+        with unittest.mock.patch(
+            "numpy.random.default_rng"
+        ) as mock_np_generator_declaration:
             rng = utils_global.get_random_number_generator()
-            self.assertEqual(mock_fnc.call_count, 1)
+            self.assertEqual(mock_np_generator_declaration.call_count, 1)
+            mock_np_generator_declaration.assert_called_with(123)
 
     @unittest.mock.patch.dict(
         os.environ, {"DATAPROFILER_SEED": "George Washington"}, clear=True
@@ -51,3 +63,9 @@ class TestGetRandomNumberGenerator(unittest.TestCase):
     def test_warning_raised(self):
         with self.assertWarnsRegex(RuntimeWarning, "Seed should be an integer"):
             rng = utils_global.get_random_number_generator()
+        with unittest.mock.patch(
+            "numpy.random.default_rng"
+        ) as mock_np_generator_declaration:
+            rng = utils_global.get_random_number_generator()
+            self.assertEqual(mock_np_generator_declaration.call_count, 1)
+            mock_np_generator_declaration.assert_called_with(None)

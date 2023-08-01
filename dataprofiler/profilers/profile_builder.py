a1d31121739a1bb9b0231d9fae58a93314856e3b
Christopher Lee <chrislee011502@gmail.com>
2023-08-01 09:29:32 -0400

    Removed redundant condition for turning on multiprocessor.

--- dataprofiler/profilers/profile_builder.py
+++ dataprofiler/profilers/profile_builder.py
@@ -2874,7 +2874,7 @@ class StructuredProfiler(BaseProfiler):
 
         # Generate pool and estimate datasize
         pool = None
-        if self.options.multiprocess.is_enabled and auto_multiprocess_toggle:
+        if auto_multiprocess_toggle:
             est_data_size = data[:50000].memory_usage(index=False, deep=True).sum()
             est_data_size = (est_data_size / min(50000, len(data))) * len(data)
             pool, pool_size = profiler_utils.generate_pool(
@@ -2995,7 +2995,7 @@ class StructuredProfiler(BaseProfiler):
         # Process and label the data
         notification_str = "Calculating the statistics... "
         pool = None
-        if self.options.multiprocess.is_enabled and auto_multiprocess_toggle:
+        if auto_multiprocess_toggle:
             pool, pool_size = profiler_utils.generate_pool(4, est_data_size)
             if pool:
                 notification_str += " (with " + str(pool_size) + " processes)"

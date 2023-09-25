0b47910e479be0f552f698e4f5677549e3d23826
Taylor Turner <taylorfturner@gmail.com>
2023-09-25 09:46:37 -0400

    Update dataprofiler/profilers/categorical_column_profile.py
    Co-authored-by: Michael Davis <36012613+micdavis@users.noreply.github.com>

--- dataprofiler/profilers/categorical_column_profile.py
+++ dataprofiler/profilers/categorical_column_profile.py
@@ -320,9 +320,7 @@ class CategoricalColumn(BaseColumnProfiler["CategoricalColumn"]):
             for iter_key in self_cat_count.keys():
                 percent_self = self_cat_count[iter_key] / self.sample_size
                 percent_other = other_cat_count[iter_key] / other_profile.sample_size
-                if (percent_other == 0) or (percent_self == 0):
-                    total_psi += 0.0
-                else:
+                if (percent_other != 0) and (percent_self != 0):
                     total_psi += (percent_other - percent_self) * math.log(
                         percent_other / percent_self
                     )

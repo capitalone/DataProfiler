8879aaa321b6453cd684b5241c866baab353ab90
Taylor Turner <taylorfturner@gmail.com>
2024-05-21 14:12:32 -0400

    Update Documentation v0.11.0 (#1141)
    * docs: fix indentation for structured_options.int.min (#1112)
    
    * docs: fix indentation for structured_options.int.min
    
    * docs: adjust spacing
    
    * docs: update copyight year (#1116)
    
    * initial commit 0.11.0
    
    * update docs 0.11.0
    
    ---------
    
    Co-authored-by: Patrick Carlson <carlson2442@gmail.com>

---
+++ docs/0.11.0/html/_sources/dataprofiler.profilers.categorical_column_profile.rst.txt
@@ -0,0 +1,7 @@
+Categorical Column Profile
+==========================
+
+.. automodule:: dataprofiler.profilers.categorical_column_profile
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ sys.path.insert(0, os.path.abspath(f'../../{branch_name}/examples'))
 # -- Project information -----------------------------------------------------
 
 project = 'Data Profiler'
-copyright = '2020, Jeremy Goodsitt, Austin Walters, Anh Truong, Grant Eden, and Chris Wallace'
+copyright = '2024, Jeremy Goodsitt, Austin Walters, Anh Truong, Grant Eden, and Chris Wallace'
 author = 'Jeremy Goodsitt, Austin Walters, Anh Truong, Grant Eden, and Chris Wallace'
 
 # The full version, including alpha/beta/rc tags

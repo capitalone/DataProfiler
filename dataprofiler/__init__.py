318b0b04c5d5d2332313d0893b91d791e545a476
Gábor Lipták <gliptak@gmail.com>
2024-06-05 11:59:52 -0500

    Replace snappy with cramjam (#1091)
    * add downloads tile (#1085)
    
    * Replace snappy with cramjam
    
    * Delete test_no_snappy
    
    ---------
    
    Co-authored-by: Taylor Turner <taylorfturner@gmail.com>

--- dataprofiler/__init__.py
+++ dataprofiler/__init__.py
@@ -20,22 +20,6 @@ from .reports import graphs
 from .validators.base_validators import Validator
 from .version import __version__
 
-try:
-    import snappy
-except ImportError:
-    import warnings
-
-    warnings.warn(
-        "Snappy must be installed to use parquet/avro datasets."
-        "\n\n"
-        "For macOS use Homebrew:\n"
-        "\t`brew install snappy`"
-        "\n\n"
-        "For linux use apt-get:\n`"
-        "\tsudo apt-get -y install libsnappy-dev`\n",
-        ImportWarning,
-    )
-
 
 def set_seed(seed=None):
     # also check it's an integer

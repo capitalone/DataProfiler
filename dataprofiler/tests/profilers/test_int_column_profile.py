aa8eaa93f1953b1a9d10c6558b19065a7a4765ed
Jeremy Goodsitt <jeremy.goodsitt@ipcopilot.ai>
2026-04-07 12:09:49 -0500

    fix: accidentally duplicated test on rebase

--- dataprofiler/tests/profilers/test_int_column_profile.py
+++ dataprofiler/tests/profilers/test_int_column_profile.py
@@ -1097,11 +1097,6 @@ class TestIntColumn(unittest.TestCase):
             profile_diff.pop("median_absolute_deviation"),
             places=2,
         )
-        self.assertAlmostEqual(
-            expected_diff.get("t-test").get("welch").pop("p-value"),
-            profile_diff.get("t-test").get("welch").pop("p-value"),
-            places=10,
-        )
         self.assertDictEqual(expected_diff, profile_diff)
 
         # Assert type error is properly called

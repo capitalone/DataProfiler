e1afcf7efdcb84c9854c96c891a61cff57a4258f
Jeremy Goodsitt <jeremy.goodsitt@ipcopilot.ai>
2026-05-22 12:04:15 -0500

    fix: int pre-commit

--- dataprofiler/profilers/int_column_profile.py
+++ dataprofiler/profilers/int_column_profile.py
@@ -163,7 +163,7 @@ class IntColumn(
         df_series = df_series.reset_index(drop=True)
         is_each_row_int = self._is_each_row_int(df_series)
         sample_size = len(is_each_row_int)
-        match_int_count = np.sum(is_each_row_int)
+        match_int_count: int = int(np.sum(is_each_row_int))
         profile = dict(match_count=match_int_count, sample_size=sample_size)
 
         BaseColumnProfiler._perform_property_calcs(
